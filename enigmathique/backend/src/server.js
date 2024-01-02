e94293fee7e8c9285a96dd4707d1074d57966e44
dupuisth <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-02 14:35:36 +0100

    chore(api): ajout dépendance pour lire body des requêtes

--- enigmathique/backend/src/server.js
+++ enigmathique/backend/src/server.js
@@ -1,20 +1,31 @@
 /**
- * Serveur simple, répond à /
+ * Point d'entrée du serveur.
  */
 
+// Importe les modules nécessaires
 const express = require("express");
+const bodyParser = require("body-parser");
 
+// Initialise l'application
 const app = express();
 
+// Utilise le middleware bodyParser pour parser les requêtes de type application/json et application/x-www-form-urlencoded (POST)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Initialise la base de données
 const db = require("./models/db.js");
 db.sequelize.sync();
 
+// Définit les routes
 require("./routes/professor.route.js")(app);
 
+// Route par défaut
 app.get("/", (req, res) => {
 	return res.status(200).json({message: "Hello, World!"});
 });
 
+// Lance le serveur
 const PORT = 5000;
 app.listen(PORT, () => {
 	console.log(`Server is running: http://localhost:${PORT}`);
