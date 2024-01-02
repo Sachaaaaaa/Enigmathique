515808002850b5f8ce36a29e0b0594efcfef2fa9
dupuisth <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-02 14:14:57 +0100

    chore(api): ajout dépendances pour db

--- enigmathique/backend/src/server.js
+++ enigmathique/backend/src/server.js
@@ -6,6 +6,9 @@ const express = require("express");
 
 const app = express();
 
+const db = require("./models/db.js");
+db.sequelize.sync();
+
 app.get("/", (req, res) => {
 	return res.status(200).json({message: "Hello, World!"});
 });
