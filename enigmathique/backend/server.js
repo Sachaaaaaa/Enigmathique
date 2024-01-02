32b3d20e9472dfc8bad304c597dc0d5650557f07
dupuisth <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-02 10:49:47 +0100

    feat: mise en place du projet avec ExpressJS et ReactJS

---
+++ enigmathique/backend/server.js
@@ -0,0 +1,16 @@
+/**
+ * Serveur simple, répond à /
+ */
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	return res.status(200).json({message: "Hello, World!"});
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+	console.log(`Server is running: http://localhost:${PORT}`);
+});
