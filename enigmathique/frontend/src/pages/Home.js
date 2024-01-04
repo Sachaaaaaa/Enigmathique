96cac496f95a06f9147bbd629db0f1ea8c1ad3ec
dupuisth <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-04 11:56:56 +0100

    feat(web): communication avec api

--- enigmathique/frontend/src/pages/Home.js
+++ enigmathique/frontend/src/pages/Home.js
@@ -1,13 +1,26 @@
 import logo from '../assets/logo.svg';
 import '../assets/App.css';
+import { useEffect, useState } from 'react';
 
 function Home() {
+	// Requête à l'API "http://localhost:5000"
+	// Afficher les données reçues
+
+	const [data, setData] = useState("loading...");
+
+	useEffect(() => {
+		fetch('http://localhost:5000')
+			.then(response => response.json())
+			.then(data => setData(data.message))
+			.catch(error => setData(error.message));
+	});
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+					Message from backend: {data}
         </p>
         <a
           className="App-link"
