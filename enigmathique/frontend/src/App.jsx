9cc46b4cf015c9c0e05f7cdd436687dad5b765b0
Thibaut DUPUIS <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-08 16:26:28 +0100

    feat(web): ajout page login

--- enigmathique/frontend/src/App.jsx
+++ enigmathique/frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home'; // le style de l'application
+import Login from './pages/Login'; // la page de connexion
 
 const App = () => {
 	return(
 		<Routes>
 			<Route path='/' element={<Home />} />
+			<Route path='/login' element={<Login />} />
 		</Routes>
 	);
 }
