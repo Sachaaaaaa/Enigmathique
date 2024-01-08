64bce5d76aaaa2bc1393b7734a91683588da9eeb
Mathéo TARDY <mattar7338@gmail.com>
2024-01-08 15:54:21 +0100

    refactor(web): single quotes

--- enigmathique/frontend/src/App.jsx
+++ enigmathique/frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Routes, Route } from 'react-router-dom';
 import './index.css';
-import Home from "./pages/Home"; // le style de l'application
+import Home from './pages/Home'; // le style de l'application
 
 function App() {
 	return(
-	<Routes>
-		<Route path="/" element={<Home />} />
-	</Routes>
+		<Routes>
+			<Route path='/' element={<Home />} />
+		</Routes>
 	);
 }
 export default App;
