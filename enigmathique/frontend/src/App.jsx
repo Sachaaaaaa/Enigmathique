365a80ed70059083cf1e86f16484868a82bab81e
Mathéo TARDY <matheo.tardy@etu.univ-grenoble-alpes.fr>
2024-01-08 17:27:10 +0100

    feat(web): ajout route Test

--- enigmathique/frontend/src/App.jsx
+++ enigmathique/frontend/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home';
-import SignUp from './pages/SignUp'; // le style de l'application
+import SignUp from './pages/SignUp';
+import Test from './pages/Test'; // le style de l'application
 
 const App = () => {
 	return(
 		<Routes>
 			<Route path='/' element={<Home />} />
 			<Route path='/SignUp' element={<SignUp />} />
+			<Route path='/test' element={<Test/>} />
 		</Routes>
 	);
 }
