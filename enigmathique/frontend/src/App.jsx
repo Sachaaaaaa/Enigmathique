055ec2f992371ef4faca754f71e850a7cd210507
Thibaut DUPUIS <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-08 20:04:14 +0100

    feat(web): route vers game

--- enigmathique/frontend/src/App.jsx
+++ enigmathique/frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Test from './pages/Test';
+import Game from './pages/Game';
 import './index.css';
 
 const App = () => {
@@ -13,7 +14,8 @@ const App = () => {
 			<Route path='/signup' element={<Signup />} />
 			<Route path='/login' element={<Login />} />
 			<Route path='/test' element={<Test/>} />
+			<Route path='/game' element={<Game/>} />
 		</Routes>
 	);
-}
+};
 export default App;
