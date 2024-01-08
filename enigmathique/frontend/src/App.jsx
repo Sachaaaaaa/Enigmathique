5ed96749f4855091b5dd3e939530764ef8075452
Mathéo TARDY <matheo.tardy@etu.univ-grenoble-alpes.fr>
2024-01-08 16:08:03 +0100

    refacto(web): fonction fléchées

--- enigmathique/frontend/src/App.jsx
+++ enigmathique/frontend/src/App.jsx
@@ -3,7 +3,7 @@ import {Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home'; // le style de l'application
 
-function App() {
+const App = () => {
 	return(
 		<Routes>
 			<Route path='/' element={<Home />} />
