28544332c415114e22315ad827f0d41e66232ae0
Yvain Dalban <yvain.dalban@etu.univ-grenoble-alpes.fr>
2024-01-09 13:29:43 +0100

    feat (web): début style dashboard

--- enigmathique/frontend/src/pages/Dashboard.jsx
+++ enigmathique/frontend/src/pages/Dashboard.jsx
@@ -4,33 +4,38 @@ import { Link } from "react-router-dom";
 const Dashboard = () => {
     return (
         <div>
-            <SideBar/>
-            <div> {/*divs à changer si nécessaire*/}
+            <div className="flex"> {/*divs à changer si nécessaire*/}
                 <h2>MON TABLEAU DE BORD</h2>
                 <img src={""} alt="user-icon"/>
             </div>
-            <article>
-                <h3>Mes parties récentes</h3>
-                <Link to=""></Link>
-                <Link to=""></Link>
-                <Link to=""></Link>
-            </article>
-            <article>
-                <h3>Mes classes</h3>
-                <Link to="">Seconde</Link>
-                <Link to="">Seconde</Link>
-                <Link to="">Seconde</Link>
-            </article>
-            <article>
-                <h3>Proposition de salles</h3>
-                <Link to="">Seconde</Link>
-                <Link to="">Seconde</Link>
-                <Link to="">Seconde</Link>
-            </article>
-            <article>
-                <h3>Meilleure classe</h3>
-                <div></div>
-            </article>
+            <div>
+                <section className="flex">
+                    <article className="w-1/2 border-2 drop-shadow-md m-1">
+                        <h3>Mes parties récentes</h3>
+                        <Link to=""></Link>
+                        <Link to=""></Link>
+                        <Link to=""></Link>
+                    </article>
+                    <article className="w-1/2 border-2 drop-shadow-md m-1">
+                        <h3>Mes classes</h3>
+                        <Link to="">Seconde</Link>
+                        <Link to="">Seconde</Link>
+                        <Link to="">Seconde</Link>
+                    </article>
+                </section>
+                <section className="flex">
+                    <article className="w-1/2 border-2 drop-shadow-md m-1">
+                        <h3>Proposition de salles</h3>
+                        <Link to="">Seconde</Link>
+                        <Link to="">Seconde</Link>
+                        <Link to="">Seconde</Link>
+                    </article>
+                    <article className="w-1/2 border-2 drop-shadow-md m-1">
+                        <h3>Meilleure classe</h3>
+                        <div></div>
+                    </article>
+                </section>
+            </div>
         </div>
 
     )
