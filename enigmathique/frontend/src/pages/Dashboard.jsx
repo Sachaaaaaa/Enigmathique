dfbef4cc6a85e9a8dbc8045aff8f602c0e3bbd37
Yvain Dalban <yvain.dalban@etu.univ-grenoble-alpes.fr>
2024-01-09 11:29:32 +0100

    feat (web): Début page dashboard

---
+++ enigmathique/frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <SideBar/>
+            <div> {/*divs à changer si nécessaire*/}
+                <h2>MON TABLEAU DE BORD</h2>
+                <img src={""} alt="user-icon"/>
+            </div>
+            <article>
+                <h3>Mes parties récentes</h3>
+                <Link to=""></Link>
+                <Link to=""></Link>
+                <Link to=""></Link>
+            </article>
+            <article>
+                <h3>Mes classes</h3>
+                <Link to="">Seconde</Link>
+                <Link to="">Seconde</Link>
+                <Link to="">Seconde</Link>
+            </article>
+            <article>
+                <h3>Proposition de salles</h3>
+                <Link to="">Seconde</Link>
+                <Link to="">Seconde</Link>
+                <Link to="">Seconde</Link>
+            </article>
+            <article>
+                <h3>Meilleure classe</h3>
+                <div></div>
+            </article>
+        </div>
+
+    )
+}
