d857295c2ef36e569a4e98b386075d1203faad2d
Mathéo TARDY <matheo.tardy@etu.univ-grenoble-alpes.fr>
2024-01-09 15:07:48 +0100

    feat(web): Création Student.jsx

--- enigmathique/frontend/src/components/Student.jsx
+++ enigmathique/frontend/src/components/Student.jsx
@@ -1,16 +1,53 @@
-import React from "react";
+import React, {useContext} from "react";
 import PropTypes from "prop-types";
+import { IoAddCircle, IoRemoveCircle } from "react-icons/io5";
+
+import AvailableContext from "./join/AvailableStudents.context";
+import SelectedContext from "./join/SelectedStudents.context";
 
 const Student = (props) => {
+
+    const {available, setAvailable} = useContext(AvailableContext);
+    const {selected, setSelected} = useContext(SelectedContext);
+    const targetStudent = {
+        name: props.name,
+        firstname: props.firstname,
+    }
+
+    const handleState = () => {
+        if (available.some(student =>
+        student.name === targetStudent.name && student.firstname === targetStudent.firstname)) {
+            setAvailable(available.filter(student =>
+                student.name !== targetStudent.name || student.firstname !== targetStudent.firstname));
+            setSelected([...selected, targetStudent]);
+        } else {
+            setSelected(selected.filter(student =>
+                student.name !== targetStudent.name || student.firstname !== targetStudent.firstname));
+            setAvailable([...available, targetStudent]);
+        }
+
+    };
+
+
     return (
-        <div>
-            <p>{props.firstname}</p>
-            <p>{props.name}</p>
+        <div className="flex flex-row items-center gap-10 justify-start p-4 bg-cyan-500 text-white rounded-xl">
+            <p className="w-56">{props.firstname}</p>
+            <p className="w-56">{props.name}</p>
+            {props.isSelected ?
+                <button className="p-2 bg-red-500 rounded-xl" onClick={handleState}>
+                    <IoRemoveCircle size={25}/>
+                </button>
+                :
+                <button className="p-2 bg-green-500 rounded-xl" onClick={handleState}>
+                    <IoAddCircle size={25}/>
+                </button>
+            }
         </div>
     );
 }
 Student.propTypes = {
     name: PropTypes.string.isRequired,
     firstname: PropTypes.string.isRequired,
+    isSelected: PropTypes.bool.isRequired,
 }
 export default Student;
