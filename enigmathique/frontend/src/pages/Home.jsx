b64330cde87ca62686d4307013c0d1674099aab2
guillevm <Matheo.Guillevic@etu.univ-grenoble-alpes.fr>
2024-01-09 10:38:17 +0100

    feat(web): ajout du formulaire pour rejoindre une partie

--- enigmathique/frontend/src/pages/Home.jsx
+++ enigmathique/frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import NavBarHome from "../components/NavBarHome";
+import HomeForm from "../components/HomeForm";
 import "../index.css";
 function Home() {
 	return (
-		<NavBarHome />
-
+		<>
+			<NavBarHome />
+			<HomeForm />
+		</>
 	);
 }
 export default Home;
