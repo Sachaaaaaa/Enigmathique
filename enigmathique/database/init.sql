ac6f570b7474851dbfdc2c9f4f3f8421637a96e7
Thibaut DUPUIS <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-09 10:42:26 +0100

    feat: ajout docker

--- enigmathique/database/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE PROFESSOR (
-  id serial PRIMARY KEY,
-  lastname varchar NOT NULL,
-  firstname varchar NOT NULL,
-  mail varchar NOT NULL UNIQUE,
-  password varchar NOT NULL,
-  date_created date NOT NULL DEFAULT CURRENT_DATE,
-  date_updated date NOT NULL DEFAULT CURRENT_DATE
-);
