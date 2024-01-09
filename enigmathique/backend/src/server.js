a77573dbd0c1b0d8282caf60f129e08f912a5e3a
Thibaut DUPUIS <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-09 08:24:07 +0100

    fix(api): ajout server pour socketio

--- enigmathique/backend/src/server.js
+++ enigmathique/backend/src/server.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors(origin = "*"));
 
 // Initialise le socket manager
+const server = http.createServer(app);
 const io = socketio(server, {
 	cors: {
 		origin: "*"
