86f8e3376b9e462a4caa79b81ccadb8490c1eec8
Sacha Wos <sacha.wos@etu.univ-grenoble-alpes.fr>
2024-01-09 10:02:28 +0100

    feat(api): ajout de l'authentification par token

--- enigmathique/backend/src/routes/middleware.js
+++ enigmathique/backend/src/routes/middleware.js
@@ -1,18 +1,32 @@
-
 const jwt = require('jsonwebtoken');
+const secretKey = 'bloubiboulba';
 
 exports.verifyToken = (req, res, next) => {
-    console.log("lllllllllllllllllllllllllllllllllllllllllllllllllllllllllll")
+
     const token = req.headers['authorization']
-    console.log(token)
-  
-    if (token == null) return res.sendStatus(401)
-  
-    console.log(jwt.verify(token, process.env.TOKEN_SECRET))
-    console.log("ll")
+
+    // Si il n'y a pas de token, indique à l'utilisateur qu'on est pas connecté
+    if(!token){
+        res.status(403).send({
+			message: "Vous n'êtes pas connecté"
+		});
+		return;
+    }
     
+
+    // Vérifie la validité du token, si il l'est on accède a la ressource demandé, sinon on retourne une erreur 403
+    try {
+        const decodedToken = jwt.verify(token, secretKey)
+        req.tokenId = decodedToken.id;
+        next()
+    } catch (error) {
+        res.status(403).send({
+			message: "Token non valide"
+		});
+		return;
+    }
 }
 
 
 
-// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtYWlsIjoic2FjaGFAZ21haWwuY29tIiwiaWF0IjoxNzA0NzMwMzkyLCJleHAiOjE3MDQ3MzIxOTJ9.pia3xPOK8-j_utjckC3GyeT_48gG2LlRGFueQ_NARas"
+// 	eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsYXN0bmFtZSI6ImxvdWlzIiwiZmlyc3RuYW1lIjoiamVhbiIsIm1haWwiOiJqZWFhYWFuYUBnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRLbHZ1V0xWRWR0b01zbGJwdW1MTzQub1ZnTkh6cFlKWEV3aGhKbUNSMFJOeGdDYlpmUzVILiIsImlhdCI6MTcwNDc4NjAxOSwiZXhwIjoxNzA0Nzg5NjE5fQ.08vZdB0yKxcHwZMrPa7hJmZl80q0mQ_76W-9NP0zYRs
