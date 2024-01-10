873aae03acbf291a24f431460927ebc4cfe178fb
briandd <da.briand04@gmail.com>
2024-01-10 11:35:05 +0100

    feat(Survole objet) : affiche nom objets survolé

--- enigmathique/frontend/src/components/SceneGLTF.jsx
+++ enigmathique/frontend/src/components/SceneGLTF.jsx
@@ -1,15 +1,25 @@
 /* eslint-disable indent */
 import React from "react";
+import { useState } from "react";
 import { Environment, OrbitControls } from "@react-three/drei";
-import { Room001 } from "./RoomGLTF-001";
+//import { Room001 } from "./RoomGLTF-001";
 import SceneLoader from "./testGLB";
 
 export const Scene = () => {
-    const glbPath = '/models/Demo_Room-002.glb';
+		const [hoveredObject, setHoveredObject] = useState(false);
 
+    const scenePath = '/models/Demo_Room-002.glb';
+
+    // Afficher les touts les objects de la scene
     const handleObjectsLoaded = (objects) => {
     console.log('Objects loaded:', objects);
-    // Faites quelque chose avec les objets de la scène
+
+  };
+
+	// Afficher les objects survolés
+	const handleHover = (object) => {
+    setHoveredObject(object ? object === "Marmite" : false);
+    console.log('Hovered object:', object);
   };
   
     return (
@@ -17,10 +27,12 @@ export const Scene = () => {
         <Environment preset="sunset" />
         <ambientLight intensity={0.3} />
         <OrbitControls />
+
         {// Comment
         //<Room001 />
         }
-        <SceneLoader glbPath={glbPath} onObjectsLoaded={handleObjectsLoaded}/>
+
+        <SceneLoader glbPath={scenePath} onObjectsLoaded={handleObjectsLoaded} onHover={handleHover}/>
         </>
     );
 };
