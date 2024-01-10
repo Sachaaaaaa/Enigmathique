c7fcaf944962e07c0b1e1ae9a1392be747c1fc1f
guillevm <Matheo.Guillevic@etu.univ-grenoble-alpes.fr>
2024-01-10 16:37:42 +0100

    fix(web): modification des popup de suppression réutilisation du même component pour les élèves et les classes

--- enigmathique/frontend/src/components/PopupDelete.jsx
+++ enigmathique/frontend/src/components/PopupDelete.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Popup from "reactjs-popup";
 import PropTypes from "prop-types";
-import '../index.css';
+import "../index.css";
 const PopupDelete = (props) => {
 	return (
-		<Popup trigger={<button className="btn-delete">supprimer l&apos;élève</button>}>
+		<Popup trigger={<button className="btn-delete">supprimer {props.type === "class" ? "la classe": "l'élève"}</button>}>
 			<div className="bg-white p-2 border-gray-500 border-opacity-100">
-				<p>Voulez vous vraiment supprimer l&apos;élève {props.firstname} {props.secondname}</p>
+				<p>Voulez vous vraiment supprimer {props.type === "class"? "la classe " + props.name : "l'élève " +props.firstname +" "+ props.secondname}</p>
 				<div className="flex">
 					<button className="btn-cancel">Annuler</button>
 					<button className="btn-validate">Valider</button>
@@ -17,8 +17,10 @@ const PopupDelete = (props) => {
 }
 
 PopupDelete.propTypes = {
-	firstname: PropTypes.string.isRequired,
-	secondname: PropTypes.string.isRequired,
+	firstname: PropTypes.string,
+	secondname: PropTypes.string,
+	name: PropTypes.string,
+	type: PropTypes.string,
 };
 
 export default PopupDelete;
