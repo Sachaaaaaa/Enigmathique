182896e3e956bfde69ded85cc0077ae487a14f5d
Yvain Dalban <yvain.dalban@etu.univ-grenoble-alpes.fr>
2024-01-10 09:04:45 +0100

    feat (web): affichage conditionnel des room dans CreationGame2

--- enigmathique/frontend/src/pages/CreationGame2.jsx
+++ enigmathique/frontend/src/pages/CreationGame2.jsx
@@ -1,42 +1,92 @@
 import React from 'react';
 import SideBar from "../components/SideBar";
 import RoomItem from "../components/creationgame2/RoomItem";
-
-
+import {useState} from "react";
 
 
 const CreationGame2 = () => {
 
-  const rooms = [
-    {
-      name: "Room1",
-      difficulty: "facile"
-    },
-    {
-      name: "Room2",
-      difficulty: "moyen"
-    },
-  ]
+    const [selected, setSelected] = useState('suit')
 
-  return (
-      <div className="flex">
-        <SideBar />
-        <div>
-          <div>
-            <h2>Séléction des salles</h2>
-              <input id="Suites" type="radio" name="chapitre" className="" checked/>
-              <label htmlFor="Suites">Suites</label>
-              <input id="Proba" type="radio" name="chapitre" className=""/>
-              <label htmlFor="Proba">Probabilités</label>
-              <input id="Fonct" type="radio" name="chapitre" className=""/>
-              <label htmlFor="Fonct">Fonctions</label>
-              <input id="Ens" type="radio" name="chapitre" className=""/>
-              <label htmlFor="Ens">Ensembles</label>
-          </div>
-          {rooms.map((room, index) => (<RoomItem key={index} name={room.name} difficulty={room.difficulty} />))}
+    const rooms = [
+        {
+            name: "Room1",
+            difficulty: "facile",
+            cat: "proba"
+        },
+        {
+            name: "Room2",
+            difficulty: "moyen",
+            cat: "suit"
+        },
+        {
+            name: "Room3",
+            difficulty: "difficile",
+            cat: "fonct"
+        }
+    ]
+
+    const handleChange = changeEvent => {
+        setSelected(changeEvent.target.value);
+    }
+
+    return (
+        <div className="flex">
+            <SideBar/>
+            <div>
+                <div>
+                    <h2>Séléction des salles</h2>
+                    <label>
+                        <input
+                            value="suit"
+                            type="radio"
+                            name="chapitre"
+                            className=""
+                            checked={selected==='suit'}
+                            onChange={handleChange}
+                        />
+                        Suites
+                    </label>
+                    <label>
+                        <input
+                            value="proba"
+                            type="radio"
+                            name="chapitre"
+                            className=""
+                            checked={selected==='proba'}
+                            onChange={handleChange}
+                        />
+                        Probabilités
+                    </label>
+                    <label>
+                        <input
+                            value="fonct"
+                            type="radio"
+                            name="chapitre"
+                            className=""
+                            checked={selected==='fonct'}
+                            onChange={handleChange}
+                        />
+                        Fonctions
+                    </label>
+                    <label>
+                        <input
+                            value="ens"
+                            type="radio"
+                            name="chapitre"
+                            className=""
+                            checked={selected==='ens'}
+                            onChange={handleChange}
+                        />
+                        Ensembles
+                    </label>
+                </div>
+                {rooms.map((room, index) => (
+                    room.cat === selected && <RoomItem key={index} name={room.name} difficulty={room.difficulty}/>
+                ))}
+            </div>
         </div>
-      </div>
-  );
+    );
 };
 
 
