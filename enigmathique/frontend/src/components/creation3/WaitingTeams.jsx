64f2c2e5a0ee0d4405bec1745e1eff042d32e769
Mathéo TARDY <matheo.tardy@etu.univ-grenoble-alpes.fr>
2024-01-10 08:08:51 +0100

    feat(web): Création components

---
+++ enigmathique/frontend/src/components/creation3/WaitingTeams.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WaitingTeams = () => {
+    return (
+        <section className="w-5/12 border-2 border-blue-800 rounded-xl p-2">
+
+        </section>
+    );
+}
+
+export default WaitingTeams;
