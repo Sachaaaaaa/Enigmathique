88fcf2fbdd6ba6c7ab4943bb3f41c9571f442c85
guillevm <Matheo.Guillevic@etu.univ-grenoble-alpes.fr>
2024-01-10 15:41:05 +0100

    fix(web): correction de la PopupDelete

--- enigmathique/frontend/src/components/PopupDelete.jsx
+++ enigmathique/frontend/src/components/PopupDelete.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import Popup from "reactjs-popup";
 import PropTypes from "prop-types";
-
+import '../index.css';
 const PopupDelete = (props) => {
 	return (
-		<Popup trigger={<button>trigger</button>}>
-			<div className="bg-white">
+		<Popup trigger={<button className="btn-delete">supprimer l&apos;élève</button>}>
+			<div className="bg-white p-2 border-gray-500 border-opacity-100">
 				<p>Voulez vous vraiment supprimer l&apos;élève {props.firstname} {props.secondname}</p>
 				<div className="flex">
-					<button
-						className="bg-red-800 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-50"
-						type="submit">
-						Annuler
-					</button>
-					<button
-						className="bg-blue-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-50"
-						type="submit">
-						valider
-					</button>
+					<button className="btn-cancel">Annuler</button>
+					<button className="btn-validate">Valider</button>
 				</div>
 			</div>
 		</Popup>
