c812319ac1fdc73831f502590d2236423ecc28eb
Thibaut DUPUIS <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-11 08:56:51 +0100

    feat(game): début chargement de scène sur demande

--- enigmathique/frontend/src/components/SceneManager.jsx
+++ enigmathique/frontend/src/components/SceneManager.jsx
@@ -4,11 +4,21 @@ import { useState, useContext } from "react";
 import { Environment, OrbitControls } from "@react-three/drei";
 
 import { Model } from "./testGLB";
-import { Room001 } from "./RoomGLTF-001";
+import { Room001 } from "./rooms/Laboratory";
 import { SocketContext } from "../context/socket";
 
 export const Scene = () => {
 	const socket = useContext(SocketContext);
+	const [roomName, setRoomName] = useState(null);
+
+	// Charge la scène en fonction de son nom
+	useEffect(() => {
+		if (roomName) {
+			import(`./rooms/${roomName}.jsx`).then((scene) => {
+				console.log(scene);
+			});
+		}
+	}, [roomName]);
 
 	useEffect(() => {
 		const onConnection = () => {
@@ -19,16 +29,24 @@ export const Scene = () => {
 			console.log('Déconnecté');
 		};
 
+		const onScene = (data) => {
+			setRoomName(data);
+		};
+
 		socket.on('connect', onConnection);
 		socket.on('disconnect', onDisconnection);
+		socket.on('scene', onScene);
 
 		socket.connect();
 
 		return () => {
 			socket.off('connect', onConnection);
 			socket.off('disconnect', onDisconnection);
+			socket.off('scene', onScene);
 		};
 	});
+
+
 
 	return (
 		<>
