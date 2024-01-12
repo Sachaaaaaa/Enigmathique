1ac2a1ea3d9c3a28207e6d46a5107b75b99e2074
Mathéo TARDY <matheo.tardy@etu.univ-grenoble-alpes.fr>
2024-01-12 23:30:40 +0100

    feat(web): Ajout données au contexte

--- enigmathique/frontend/src/components/contexts/CreationGame.context.jsx
+++ enigmathique/frontend/src/components/contexts/CreationGame.context.jsx
@@ -2,16 +2,42 @@ import React, { createContext, useContext, useState } from 'react';
 import PropTypes from "prop-types";
 
 const CreationGameContext = createContext(null);
-
+export const initialFormData = {
+	gameName: '',
+	course: 0,
+	teamSize: 1,
+}
+export const initialFilterData = {
+	text:'',
+	type:'suit',
+}
 export const CreationGameDataProvider = (props) => {
-	const [step, setStep] = useState(1);
-	const [formData, setFormData] = useState({gameName:'', course:0, teamSize:1});
+
+	//Data étape 1
+	const [step, setStep] = useState(1); //Étape de création
+	const [formData, setFormData] = useState(initialFormData); //Nom de la game + Classe sélectionnée + Taille équipe
+	const [courses, setCourses] = useState([{}]); //Classes du prof
+
+	//Data étape 2
+	const [rooms, setRooms] = useState([]); //Id des rooms sélectionnées
+	const [filter, setFilter] = useState(initialFilterData); //Filtres sélectionnés
+
+	//Data pregame
+	const [teams, setTeams] = useState([{}]);
 
 	const data = {
 		step,
 		setStep,
 		formData,
 		setFormData,
+		courses,
+		setCourses,
+		rooms,
+		setRooms,
+		teams,
+		setTeams,
+		filter,
+		setFilter
 	};
 
 	return (
@@ -28,3 +54,4 @@ CreationGameDataProvider.propTypes = {
 export const useCreationGameContext = () => {
 	return useContext(CreationGameContext);
 };
+
