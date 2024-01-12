bb3573b647fcbea67e377977b4d3f73ff7315603
Mathéo TARDY <matheo.tardy@etu.univ-grenoble-alpes.fr>
2024-01-12 09:32:40 +0100

    feat(web): ajout du LayoutProf

--- enigmathique/frontend/src/pages/Class.jsx
+++ enigmathique/frontend/src/pages/Class.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import SideBar from '../components/SideBar';
 import ListClass from '../components/ListClass';
+import LayoutProf from "../layouts/LayoutProf";
 
 function Class() {
 	return (
-		<div className="flex">
-			<SideBar />
-			<ListClass />
-		</div>
+		<LayoutProf>
+			<main>
+				<ListClass />
+			</main>
+		</LayoutProf>
 	);
 }
 export default Class;
--- enigmathique/frontend/src/pages/Students.jsx
+++ enigmathique/frontend/src/pages/Students.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import SideBar from "../components/SideBar";
 import ListStudents from "../components/ListStudents";
 import {useParams} from "react-router-dom";
+import LayoutProf from "../layouts/LayoutProf";
 
 function Class() {
 	const {id} = useParams()
 	return (
-		<div className="flex">
-			<SideBar/>
-			<ListStudents id={parseInt(id)}/>
-		</div>
+		<LayoutProf>
+			<main>
+				<ListStudents id={parseInt(id)}/>
+			</main>
+		</LayoutProf>
 	);
 }
 export default Class;
