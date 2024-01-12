7a8dbf920e8e2ccb97bd9cf53faf25882d3b7c5f
Thibaut DUPUIS <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-12 11:26:50 +0100

    fix(game): supprime reste merge

--- enigmathique/frontend/src/components/SceneManager.jsx
+++ enigmathique/frontend/src/components/SceneManager.jsx
@@ -1,18 +1,9 @@
 /* eslint-disable indent */
-<<<<<<< HEAD
 import React from 'react';
 import { useState, useContext, useEffect } from 'react';
 import { Environment, OrbitControls } from '@react-three/drei';
 
 import { Model } from './testGLB';
-=======
-import React, { useEffect } from 'react';
-import { useState, useContext } from 'react';
-import { Environment, OrbitControls } from '@react-three/drei';
-
-import { Model } from './testGLB';
-import { Room001 } from './RoomGLTF-001';
->>>>>>> fc267e90b21a1edaee5cb7f258723f4a35bd9f0e
 import { SocketContext } from '../context/socket';
 
 export const Scene = () => {
@@ -65,15 +56,7 @@ export const Scene = () => {
 			<Environment preset="sunset" />
 			<ambientLight intensity={0.4} />
 			<OrbitControls />
-<<<<<<< HEAD
 			{roomComponent}
-=======
-
-			{/* Mettre la scène ici
-			<Room001 />*/}
-			<Model /> 
-			
->>>>>>> fc267e90b21a1edaee5cb7f258723f4a35bd9f0e
 		</>
 	);
 };
