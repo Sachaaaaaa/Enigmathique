3e7bdcee20eac1c8380a6a2c71a92e3c1d0158ce
guillevm <Matheo.Guillevic@etu.univ-grenoble-alpes.fr>
2024-01-12 09:17:37 +0100

    fix(web): correction de des popups

--- enigmathique/frontend/src/components/ListClass.jsx
+++ enigmathique/frontend/src/components/ListClass.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import CourseService from '../services/course.service';
 import Modal, {ModalBody, ModalHeader} from './Modal';
-import {MdOutlineModeEdit} from 'react-icons/md';
+import {MdDeleteForever, MdOutlineModeEdit} from 'react-icons/md';
 import {CiSquareMore} from 'react-icons/ci';
 
 const ListClass = () => {
@@ -17,6 +17,8 @@ const ListClass = () => {
 	}, []);
 
 	const [editModalOpen, setEditModalOpen] = useState(false);
+	const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+	// TODO : Ajouter ce qu'il faut pour la suppression d'une classe
 
 
 	return (
@@ -25,30 +27,44 @@ const ListClass = () => {
 				{courses.map((classe) => (
 					<li key={classe.id} value={classe.name} className='bg-blue-700 flex h-10 p-1'>
 						<h3 className='w-40 text-center'>{classe.name}</h3>
+						<button className='btn-utils-student' onClick={() => setEditModalOpen(true)}><MdOutlineModeEdit
+							color='white' size='1.5em'/></button>
 						<Link to={`/class/${classe.id}`}>
-							<button className='btn-utils-student'><MdOutlineModeEdit/></button>
+							<button className='btn-utils-student'><CiSquareMore color='white' size='1.5em'/></button>
 						</Link>
-						<Link to='/*'>
-							<button className='btn-utils-student'><CiSquareMore/></button>
-						</Link>
-
+						<button className='btn-utils-student' onClick={() => setDeleteModalOpen(true)}>
+							<MdDeleteForever color='white' size='1.5em'/></button>
+						{editModalOpen && (
+							<Modal setOpenModal={setEditModalOpen}>
+								<ModalHeader>
+									<h1 className='text-3xl text-center'>Modifier la classe</h1>
+								</ModalHeader>
+								<ModalBody>
+									<form className='flex flex-col space-y-5'>
+										<label htmlFor='name'>Nom de la classe</label>
+										<input type='text' name='name' id='name' className='border-2 border-blue-900 rounded-md'/>
+										<button className='btn-delete'>Annuler</button>
+										<button className='btn-validate'>Valider la modification</button>
+									</form>
+								</ModalBody>
+							</Modal>)}
+						{deleteModalOpen && (
+							<Modal setOpenModal={setDeleteModalOpen} height='300'>
+								<ModalHeader>
+									<h1 className='text-3xl text-center'>Voulez vous vraiment surpprimer
+										la classe {classe.name}</h1>
+								</ModalHeader>
+								<ModalBody>
+									<form className='flex flex-col space-y-5'>
+										<button className='btn-delete'>Annuler</button>
+										<button className='btn-validate'>Valider la suppression</button>
+									</form>
+								</ModalBody>
+							</Modal>)}
 					</li>
 				))}
 			</ul>
-			{editModalOpen && (
-				<Modal setOpenModal={setEditModalOpen}>
-					<ModalHeader>
-						<h1 className='text-3xl text-center'>Modifier la classe</h1>
-					</ModalHeader>
-					<ModalBody>
-						<form className='flex flex-col space-y-5'>
-							<label htmlFor='name'>Nom de la classe</label>
-							<input type='text' name='name' id='name' className='border-2 border-blue-900 rounded-md'/>
-							<button className='btn-delete'>Annuler</button>
-							<button className='btn-validate'>Valider la modification</button>
-						</form>
-					</ModalBody>
-				</Modal>)}
+
 		</>
 	);
 };
--- enigmathique/frontend/src/components/ListStudents.jsx
+++ enigmathique/frontend/src/components/ListStudents.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Modal, {ModalBody, ModalHeader} from './Modal';
 import PropTypes from 'prop-types';
 import StudentService from '../services/student.service';
-import {MdOutlineModeEdit} from 'react-icons/md';
+import {MdDeleteForever, MdOutlineModeEdit} from 'react-icons/md';
 import {ImStatsDots} from 'react-icons/im';
 
 const ListStudents = (props) => {
@@ -28,11 +28,13 @@ const ListStudents = (props) => {
 			<ul className='bg-blue-300 space-x-10 flex flex-wrap'>
 				{students.map((student) => (
 					<li key={student.id} value={student.firstname} className='bg-gray-200 flex-col space-y-3 p-1 h-[20%]'>
-						<h3 className='w-40 text-center'>{student.firstname} {student.secondname}</h3>
+						<h3 className='w-40 text-center'>{`${student.firstname} ${student.lastname}`}</h3>
 						<h3 className='w-40 text-center'>{student.class}</h3>
 						<button className='btn-utils-student'><ImStatsDots color='white' size='1.5em'/></button>
 						<button className='btn-utils-student' onClick={() => setEditModalOpen(true)}>
 							<MdOutlineModeEdit color='white' size='1.5em'/></button>
+						<button className='btn-utils-student' onClick={() => setdeleteModalOpen(true)}>
+							<MdDeleteForever color='white' size='1.5em'/></button>
 						{editModalOpen && (
 							<Modal setOpenModal={setEditModalOpen}>
 								<ModalHeader>
@@ -43,8 +45,7 @@ const ListStudents = (props) => {
 										<label htmlFor='firstname'>Prénom</label>
 										<input type='text' name='firstname' id='firstname' className='border-2 border-blue-900 rounded-md'/>
 										<label htmlFor='secondname'>Nom</label>
-										<input type='text' name='secondname' id='secondname'
-										       className='border-2 border-blue-900 rounded-md'/>
+										<input type='text' name='secondname' id='secondname' className='border-2 border-blue-900 rounded-md'/>
 										<button className='btn-delete'>Annuler</button>
 										<button className='btn-validate'>Valider la modification</button>
 									</form>
@@ -53,8 +54,8 @@ const ListStudents = (props) => {
 						{deleteModalOpen && (
 							<Modal setOpenModal={setdeleteModalOpen} height='300'>
 								<ModalHeader>
-									<h1 className='text-3xl text-center'>Voulez vous vraiment surpprimer
-										l&apos;élève {student.firstname} {student.secondname}</h1>
+									<h1 className='text-3xl text-center'>{`Voulez vous vraiment surpprimer
+										l'élève ${student.firstname} ${student.lastname}`}</h1>
 								</ModalHeader>
 								<ModalBody>
 									<form className='flex flex-col space-y-5'>
