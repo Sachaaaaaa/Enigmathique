d1b8bf0216d99248158be619c3a304b3e8bedab5
guillevm <Matheo.Guillevic@etu.univ-grenoble-alpes.fr>
2024-01-12 16:36:47 +0100

    feat(web): lien entre l'API et le FRONT pour la gestion des élèves

--- enigmathique/frontend/src/services/student.service.js
+++ enigmathique/frontend/src/services/student.service.js
@@ -13,7 +13,37 @@ const get = (id) => {
 		});
 }
 
-const CourseService = {
-	get
+const deleteId = (id) => {
+	const token = authHeader();
+	// Envoie une requête au serveur pour créer une nouvelle classe
+	return axios
+		.delete(API_URL + 'student/' + id, {headers: token})
+		.then((response) => {
+			return response.data;
+		});
 }
-export default CourseService;
+const create = (firstname, secondname, idCourse) => {
+	const token = authHeader();
+	// Envoie une requête au serveur pour créer une nouvelle classe
+	return axios
+		.post(API_URL + 'course', {firstname: firstname, secondname:secondname, idCourse:idCourse }, {headers: token})
+		.then((response) => {
+			return response.data;
+		});
+}
+const edit = (firstname, secondname, idCourse, id) => {
+	const token = authHeader();
+	// Envoie une requête au serveur pour créer une nouvelle classe
+	return axios
+		.put(API_URL + 'course/' + id, {firstname: firstname, secondname:secondname, idCourse:idCourse }, {headers: token})
+		.then((response) => {
+			return response.data;
+		});
+}
+const StudentService = {
+	get,
+	create,
+	deleteId,
+	edit,
+}
+export default StudentService;
