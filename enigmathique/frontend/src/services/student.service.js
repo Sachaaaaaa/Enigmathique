1a3b1732db2833aee3c6dc498ea50aecf1e21126
guillevm <Matheo.Guillevic@etu.univ-grenoble-alpes.fr>
2024-01-12 15:27:27 +0100

    feat(web): lien entre l'API et le FRONT pour la gestion des classes

--- enigmathique/frontend/src/services/student.service.js
+++ enigmathique/frontend/src/services/student.service.js
@@ -5,10 +5,9 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 const get = (id) => {
 	const token = authHeader();
-	console.log('token', token);
 	// Envoie une requête au serveur pour créer une nouvelle classe
 	return axios
-		.post(API_URL + 'student/' + id, {idCourse: id}, {headers: token})
+		.get(API_URL + 'course/' + id, {headers: token})
 		.then((response) => {
 			return response.data;
 		});
