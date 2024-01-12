1746e357ee4527c59972bb2370dcfae109c3cf98
Sacha Wos <sacha.wos@etu.univ-grenoble-alpes.fr>
2024-01-12 14:08:55 +0100

    fix(api): Ajout récupération des élèves d'une classe

--- enigmathique/backend/src/server.js
+++ enigmathique/backend/src/server.js
@@ -35,9 +35,8 @@ require("./routes/professor.route.js")(app);
 require("./routes/course.route.js")(app);
 require("./routes/student.route.js")(app);
 require("./routes/authentification.route.js")(app);
-require("./routes/game.route.js")(app);
 require("./routes/team.route.js")(app);
-
+require("./routes/game.route.js")(app);
 
 // Route par défaut
 app.get("/", (req, res) => {
