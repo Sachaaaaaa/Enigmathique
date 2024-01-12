cb988db22e389ab6978d2ea1f36df65b44880518
Mathéo TARDY <matheo.tardy@etu.univ-grenoble-alpes.fr>
2024-01-12 09:33:05 +0100

    feat(web): ajout TopBar au LayoutProf

---
+++ enigmathique/frontend/src/components/TopBar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { FaGear } from "react-icons/fa6";
+import {useLocation} from "react-router-dom";
+const TopBar = () => {
+
+	const location = useLocation();
+	const path = location.pathname.split("/");
+	path.shift();
+
+
+	const prof = {
+		firstname: "Philippe",
+		lastname: "Lacherez",
+	} // faire une requete
+
+	const textMap ={
+		dashboard: "Tableau de bord",
+		class: "Mes classes",
+		game: "Mes parties",
+		room: "Salles d'énigmes",
+	}
+
+	let text = textMap[path[0]];
+
+	if (path.length === 2) {
+		switch (path[0]) {
+			case "class":
+				text = "nomclasse" //faire requete sur api;
+		}
+	}
+
+
+	return(
+		<section className="flex flex-row p-6 items-center border-b-2 border-[#E6EFF5]">
+			<div className="w-11/12">
+				<h1 className="text-3xl">{text}</h1>
+			</div>
+			<div className="flex flex-row items-center gap-2">
+				<div className="p-2 rounded-full bg-[#E6EFF5]">
+					<FaGear/>
+				</div>
+				<div className="p-2 text-right">
+					<p>{prof.firstname}</p>
+					<p>{prof.lastname}</p>
+				</div>
+				<img src="https://placehold.co/40" alt="profile picture" className="rounded-full p-2"/>
+			</div>
+		</section>
+	);
+}
+
+export default TopBar;
--- enigmathique/frontend/src/layouts/LayoutProf.jsx
+++ enigmathique/frontend/src/layouts/LayoutProf.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import SideBar from "../components/SideBar";
+import TopBar from "../components/TopBar";
 
 const LayoutProf = (props) => {
 	return(
 		<div className="flex flex-row h-screen w-screen">
 			<SideBar/>
-			{props.children}
+			<div className="flex flex-col w-full h-full">
+				<TopBar/>
+				{props.children}
+			</div>
 		</div>
 	);
 }
