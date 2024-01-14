7379865dcd93bfc7a23bac587a03837fa24757d0
guillevm <matheoguillevic@gmail.com>
2024-01-14 21:28:52 +0100

    fix(web): changement de l'URL de l'api pour récupérer la liste des élèves d'une classe

--- enigmathique/frontend/src/services/student.service.js
+++ enigmathique/frontend/src/services/student.service.js
@@ -8,8 +8,9 @@ const get = (id) => {
 	const token = authHeader();
 	// Envoie une requête au serveur pour créer une nouvelle classe
 	return axios
-		.get(API_URL + 'course/' + id, {headers: token})
+		.get(API_URL + 'course/students/' + id, {headers: token})
 		.then((response) => {
+			console.log(response.data);
 			return response.data;
 		});
 }
