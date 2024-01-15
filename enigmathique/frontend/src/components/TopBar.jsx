fccdf7fc79bbf8e29729b376892a09f5e528d58a
Raph255 <raphael.pivot2505@gmail.com>
2024-01-15 12:51:59 +0100

    style (web) : topbar réduite

--- enigmathique/frontend/src/components/TopBar.jsx
+++ enigmathique/frontend/src/components/TopBar.jsx
@@ -68,19 +68,19 @@ const TopBar = () => {
 
 
 	return(
-		<section className="topbar-container">
-			<div className="w-11/12">
-				<h1 className="text-3xl">{text}</h1>
+		<section className="topbar-container p-0 pl-5">
+			<div className="w-[85%]">
+				<h1 className="text-slate-700 text-2xl font-semibold">{text}</h1>
 			</div>
 			<div className="flex flex-row items-center gap-2">
 				<div className="p-2 rounded-full bg-[#E6EFF5]">
 					<FaGear/>
 				</div>
-				<div className="p-2 text-right">
+				<div className="px-0 py-2 text-right text-xs">
 					<p>{prof.firstname}</p>
 					<p>{prof.lastname}</p>
 				</div>
-				<img src="https://placehold.co/40" alt="profile picture" className="rounded-full p-2"/>
+				<img src="https://placehold.co/40" alt="profile picture" className="rounded-full pl-1 pr-2 pt-2 pb-2"/>
 			</div>
 		</section>
 	);
