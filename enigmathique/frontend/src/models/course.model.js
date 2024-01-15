6e079a51a2d173e44c772bda09e227d392222a64
guillevm <Matheo.Guillevic@etu.univ-grenoble-alpes.fr>
2024-01-15 10:15:45 +0100

    feat(web): modèle encapsulation des service getters

---
+++ enigmathique/frontend/src/models/course.model.js
@@ -0,0 +1,39 @@
+import CourseService from "../services/course.service";
+
+class Course {
+	/**
+	 *
+	 * @param id
+	 * @param name
+	 * @param idProfessor
+	 * @param createdAt
+	 * @param updatedAt
+	 */
+	constructor(id, name, idProfessor, createdAt, updatedAt) {
+		this.id = id;
+		this.name = name;
+		this.idProfessor = idProfessor;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+
+	static async get(id) {
+		try {
+			const data = await CourseService.getOne(id);
+			return new Course(data.id, data.name, data.idProfessor, data.createdAt, data.updatedAt);
+		}catch (error){
+			console.log(error);
+		}
+	}
+	static async getAll() {
+		try {
+			const data = await CourseService.getAll();
+			return data.map((course) => new Course(course.id, course.name, course.idProfessor, course.createdAt, course.updatedAt));
+		}catch (error){
+			console.log(error);
+		}
+	}
+
+}
+
+export default Course;
