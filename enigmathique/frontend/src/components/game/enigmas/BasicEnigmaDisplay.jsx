6c7210e69df72fb002c5d8b98e3b105aa7de8ae3
briandd <da.briand04@gmail.com>
2024-01-15 16:11:40 +0100

    style(game): ajout de styles dans le jeu

--- enigmathique/frontend/src/components/game/enigmas/BasicEnigmaDisplay.jsx
+++ enigmathique/frontend/src/components/game/enigmas/BasicEnigmaDisplay.jsx
@@ -2,26 +2,43 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 
-const BasicDisplayTemplate = ({handleSubmitAnswer, hint, title, description, image}) => {
+const BasicDisplayTemplate = ({ handleSubmitAnswer, hint, title, description, image }) => {
 	const [userAnswer, setUserAnswer] = useState('');
-	
+
 	const handleInputChange = (event) => {
 		setUserAnswer(event.target.value);
 	};
-	
-	
+
+
 	return (
 		<>
 			<h1>{title}</h1>
 			<p>{description}</p>
-			{image!=null && <img src={image} alt='enigma image'/>}
+			{image != null && <img src={image} alt='enigma image' />}
 			<input
 				type="text"
+				placeholder="Votre réponse"
 				value={userAnswer}
 				onChange={handleInputChange}
-				className="m-1.5" />
-			<button onClick={() => null} className="m-1.5">Indice</button>
-			<button onClick={() => handleSubmitAnswer(userAnswer)} className="m-1.5">Essayer</button>
+				className="m-1.5"
+				style={{border: '2px solid #b3b3b3'}} 
+			/>
+			
+			<button onClick={() => null} className="m-1.5"
+				style={{
+					background: '#ffcc00',
+					padding: '8px',
+					borderRadius: '8px',
+					width: '12vw'
+				}}>Indice</button>
+
+			<button onClick={() => handleSubmitAnswer(userAnswer)} className="m-1.5"
+				style={{
+					background: '#00ff00',
+					padding: '8px',
+					borderRadius: '8px',
+					width: '12vw'
+				}}>Essayer</button>
 		</>
 	);
 };
