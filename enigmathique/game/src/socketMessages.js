4e84e052caa0c648a9727f3fe2df21eb12ab6658
Thibaut DUPUIS <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-15 21:56:48 +0100

    feat(game/back): ajout messages utiles à la composition des équipes

--- enigmathique/game/src/socketMessages.js
+++ enigmathique/game/src/socketMessages.js
@@ -7,6 +7,7 @@ const ServerToClient = {
 	Connection: 'connect',
 	Disconnection: 'disconnect',
 	
+	/** GAME */
 	// Equipe
 	SwitchRoom: 'room', // + {roomTag: string}
 	Message: 'message', // + {message: string}
@@ -14,10 +15,16 @@ const ServerToClient = {
 	TeamCount: 'teamCount', // + {count: int}
 	Feedback: 'feedback', // + {isSolved: bool, ~endMessage: string}
 	Hint: 'hint', // + {hint: string}
-
 	// Professeur
 	TeamProgress: 'teamProgress', // + {teamId: int, progress: object}
 	AllTeamsProgress: 'allTeamsProgress', // + {teamsProgress: object}
+
+	/** TEAM COMPOSITION */
+	SyncAvailableStudents: 'syncAvailableStudents', // + {students: array[]}
+	// Equipe
+	SyncTeamStudents: 'syncTeamStudents', // + {students: array[]}
+	// Professeur
+	SyncTeams: 'syncTeams', // + {teams: array[]}
 };
 
 // Messages que le client envoie au serveur
@@ -25,9 +32,22 @@ const ClientToServer = {
 	Connection: 'connect', // + {roomTag: string} => dans le query
 	Disconnection: 'disconnect', // { }
 	Message: 'message', // + {message: string}
+
+	/** GAME */
+	// Equipe
 	Submit: 'submit', // + {answer: string}
 	AskHint: 'hint', // { }
 	RoomLoaded: 'roomLoaded', // { }
+
+	/** TEAM COMPOSITION */
+	// Equipe
+	AddStudent: 'addStudent', // + {studentId: int}
+	RemoveStudent: 'removeStudent', // + {studentId: int}
+	ConfirmTeam: 'confirmTeam', // { }
+
+	// Professeur
+	ValidateTeam: 'validateTeam', // + {teamId: int}
+	RefuseTeam: 'refuseTeam', // + {teamId: int}
 };
 
 // Types de connexion
