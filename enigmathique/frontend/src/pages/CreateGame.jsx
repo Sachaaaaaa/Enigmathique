1b1cdb2ffa1278335a36b5c111e4f1582a400a41
Mathéo TARDY <matheo.tardy@etu.univ-grenoble-alpes.fr>
2024-01-15 21:08:14 +0100

    fix(web): Contexte -> useState

--- enigmathique/frontend/src/pages/CreateGame.jsx
+++ enigmathique/frontend/src/pages/CreateGame.jsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import LayoutProf from "../layouts/LayoutProf";
 import CreationGame1 from "../components/createGame/1/CreationGame1";
 import ProgressBar from "../components/createGame/ProgressBar";
-import {useCreationGameContext} from "../components/contexts/CreationGame.context";
+import {initialFormData, useCreationGameContext} from "../components/contexts/CreationGame.context";
 import CreationGame2 from "../components/createGame/2/CreationGame2";
-
+import CourseService from "../services/course.service";
+import RoomService from "../services/room.service";
+import AuthService from "../services/auth.service";
 
 const CreateGame = () => {
 
-	//const [step, setStep] = useState(1);
-	const {step} = useCreationGameContext();
+	const {setCourses, setRooms, formData, setFormData} = useCreationGameContext();
+
+
+
+	const [step, setStep] = useState(1); //Étape de création
+
+	useEffect(() => {
+
+		AuthService.login("admin@admin.com", "admin").then((response) => {
+			CourseService.getAll().then((response) => {
+				setCourses(response);
+			}).catch((error) => {
+				console.log(error);
+			});
+			RoomService.getAllRooms().then((response) => {
+				setRooms(response);
+			}).catch((error) => {
+				console.log(error);
+			});
+		}).catch((error) => {
+			console.log(error);
+		});
+		return () => {
+            setFormData(initialFormData); // Réinitialise les données du formulaire
+        };
+	}, []);
 
 	const stepComponent = {
-		1: <CreationGame1/>,
-		2: <CreationGame2/>
+		1: <CreationGame1 setStep={setStep}/>,
+		2: <CreationGame2 setStep={setStep}/>
 	}
 
 	return (
