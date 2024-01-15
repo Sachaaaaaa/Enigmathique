ea9dfe3bd81b7f37601d5ad5856a3741c9449db8
Yvain Dalban <yvain.dalban@etu.univ-grenoble-alpes.fr>
2024-01-15 08:15:19 +0100

    feat (web): calcul stats global

--- enigmathique/frontend/src/components/stats/ScoreTeam.jsx
+++ enigmathique/frontend/src/components/stats/ScoreTeam.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 const ScoreTeam = (props) => {
 
 	const rooms = props.rooms;
+
+	const [option, setOption] = useState('global');
+
+	const handleOption = (e) => {
+		let room = rooms[0];
+		setOption(e.target.value);
+	}
+
+	const getRoom = (roomName, rooms) => {
+		if(roomName === 'global') {
+			return {
+				name: 'global',
+				time: secondsToTime(rooms.reduce((sum, room) => sum + timeToInt(room.time))),
+				score: {
+					nbHints: rooms.reduce((sum, room) => (sum + room.score.nbHints), 0),
+					nbMis: rooms.reduce((sum, room) => (sum + room.score.nbMis), 0)
+					//TODO: Résoudre le problème avec les reduces
+				}
+			}
+		} else {
+			let i = 0;
+			let theRoom = rooms[i];
+			while (theRoom.name !== roomName) {
+				i++;
+				theRoom = rooms[i];
+			}
+			return theRoom;
+		}
+	}
+
+	const timeToInt = (time) => {
+		const splitTime = time.split(':');
+		return parseInt(splitTime[0]) * 60 + parseInt(splitTime[1]);
+	}
+
+	const secondsToTime = (seconds) => {
+		const minAndSec = [Math.floor(seconds/60), seconds%60]
+		return minAndSec[0].toString().concat(':',minAndSec[1].toString())
+	}
 
 	return (
 		<div>
@@ -11,7 +50,7 @@ const ScoreTeam = (props) => {
 				<span>Statistiques</span>
 				<div>
 				<h3>Score</h3>
-					<select>
+					<select onChange={handleOption}>
 						<option value='global'>Global</option>
 						{rooms.map((room, index) => (
 							<option key={index} value={room.name}>{room.name}</option>
@@ -23,17 +62,17 @@ const ScoreTeam = (props) => {
 				<section>
 					<img src='' alt='logo'/>
 					<p>Indices utilisés</p>
-					<p>{props.rooms[0].score.nbHints}</p>
+					<p>{getRoom(option, rooms).score.nbHints}</p>
 				</section>
 				<section>
 					<img src='' alt='logo'/>
 					<p>Erreurs commises</p>
-					<p>{props.rooms[0].score.nbMis}</p>
+					<p>{getRoom(option, rooms).score.nbMis}</p>
 				</section>
 				<section>
 					<img src='' alt='logo'/>
 					<p>Salles réussies</p>
-					<p>-1 sur {props.rooms.length}</p>
+					<p>-1 sur {rooms.length}</p>
 				</section>
 			</div>
 		</div>
