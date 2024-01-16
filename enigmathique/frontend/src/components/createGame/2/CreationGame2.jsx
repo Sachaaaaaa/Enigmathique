be456002123647da34471fab058bbb80ea2438ba
guillevm <Matheo.Guillevic@etu.univ-grenoble-alpes.fr>
2024-01-16 15:08:26 +0100

    fix(web): implémentation dans l'étape de la création de la game

--- enigmathique/frontend/src/components/createGame/2/CreationGame2.jsx
+++ enigmathique/frontend/src/components/createGame/2/CreationGame2.jsx
@@ -6,6 +6,7 @@ import RoomNav from "./RoomNav";
 import Room from "./Room";
 import GameService from "../../../services/game.service";
 import PropTypes from "prop-types";
+import Game from "../../../models/game.model";
 const CreationGame2 = (props) => {
 
 	const {formData, setFormData, rooms} = useCreationGameContext();
@@ -48,19 +49,21 @@ const CreationGame2 = (props) => {
 	}
 
 	const createGame = async () => {
-		return await GameService.createGame(formData.course, formData.gameName, formData.teamSize);
+		//return await GameService.createGame(formData.course, formData.gameName, formData.teamSize);
+		return await Game.create(formData.course, formData.gameName, formData.teamSize)
 	}
 	const addRooms = async (gameId, roomsIds) => {
-		return await GameService.addRooms(gameId, roomsIds);
+		//return await GameService.addRooms(gameId, roomsIds);
+		return await Game.addRooms(gameId, roomsIds);
 	}
 	const openGame = async (gameId) => {
-		return await GameService.openGame(gameId);
+		//return await GameService.openGame(gameId);
+		return await Game.openGame(gameId);
 	}
 
 	const handleRoomSelection = (roomName) => {
 		if (selectedRooms.includes(roomName)) {
 			setSelectedRooms(selectedRooms.filter((name) => name !== roomName));
-
 		} else {
 			setSelectedRooms([...selectedRooms, roomName]);
 		}
