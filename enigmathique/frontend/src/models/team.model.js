efda328dbde3c7f834b1804e1dcccd63dac3f41e
guillevm <matheoguillevic@gmail.com>
2024-01-17 21:41:56 +0100

    feat(web): création du modèles des team et ajout d'une méthode des services

--- enigmathique/frontend/src/models/team.model.js
+++ enigmathique/frontend/src/models/team.model.js
@@ -12,7 +12,24 @@ class TeamModel{
 		return data.map((student) => new Student(student.name, student.id));
 		*/
 		try {
+			const data = await TeamService.getStudents(idTeam);
+			console.log(data);
 			return TeamService.getStudents(idTeam);
+		}catch (e){
+			console.error(e);
+		}
+	}
+	static async getScores(idTeam){
+		try {
+			return await TeamService.getScores(idTeam);
+		}catch (e){
+			console.error(e);
+		}
+	}
+	static async getTeam(idTeam){
+		try {
+			const data = await TeamService.getTeam(idTeam);
+			return new TeamModel(data.name, data.id);
 		}catch (e){
 			console.error(e);
 		}
