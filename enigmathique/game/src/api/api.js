b1289193adbf6b843fda9f0c15dd4d8347112110
Thibaut DUPUIS <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-18 17:15:02 +0100

    feat(game/back): envoie des scores à la fin de la partie

--- enigmathique/game/src/api/api.js
+++ enigmathique/game/src/api/api.js
@@ -9,7 +9,7 @@ class ApiService {
 		const response = await axios({
 			method,
 			url: API_URL + endpoint,
-			data,
+			data: data,
 			headers: {
 				'Authorization': `${token}`
 			}
@@ -116,9 +116,17 @@ class ApiService {
 		}
 	}
 
-	static async postTeamsScore(sessionId, scores) {
-		// TODO: Envoyer la requête à l'API (lorsque route implémentée)
-		return null;
+	static async postTeamsScore(sessionData) {
+		const endpoint = '/team/score';
+
+		try {
+			const response = await this.sendRequest('POST', endpoint, sessionData);
+			console.log(response);
+			return response;
+		} catch (error) {
+			console.log(error);
+			return null;
+		}
 	}
 
 	static async postSessionEnd(sessionId) {
