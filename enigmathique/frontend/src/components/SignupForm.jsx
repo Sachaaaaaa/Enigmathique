eeb2c4453575b5a8f1f12285274406b3e199ad41
Mathéo TARDY <matheo.tardy@etu.univ-grenoble-alpes.fr>
2024-01-19 11:15:57 +0100

    fix(front): bouton mot de passe

--- enigmathique/frontend/src/components/SignupForm.jsx
+++ enigmathique/frontend/src/components/SignupForm.jsx
@@ -114,6 +114,7 @@ const SignupForm = () => {
 								className='absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5'
 								onMouseDown={() => setShowPassword(true)}
 								onMouseUp={() => setShowPassword(false)}
+								type="button"
 							>
 								{showPassword ? 'Hide' : 'Show'} Password
 							</button>
