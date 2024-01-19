2043f192edf1428feec5450a999e33611d5837ed
Mathéo TARDY <matheo.tardy@etu.univ-grenoble-alpes.fr>
2024-01-19 11:28:10 +0100

    feat(front): log quand les données sont chargées

--- enigmathique/frontend/src/hooks/useGames.jsx
+++ enigmathique/frontend/src/hooks/useGames.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import {useState, useEffect} from 'react';
 import Game from 'models/game.model';
 
 /**
  * Récupère toutes les parties du professeur connecté
- * @param {boolean} autoload 
+ * @param {boolean} autoload
+ * @returns
  */
 const useGames = (autoload = true) => {
 	const [games, setGames] = useState([]);
@@ -16,7 +16,7 @@ const useGames = (autoload = true) => {
 
 	useEffect(() => {
 		if (autoload) {
-			loadGames();
+			loadGames().then(r => console.log('games loaded'));
 		}
 	}, []);
 
