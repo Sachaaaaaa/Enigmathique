d9481742fae5bc7383f156fe182e2f4ea8495828
Thibaut DUPUIS <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-21 16:44:16 +0100

    feat(api): route game/maxTeamsSize

--- enigmathique/backend/src/routes/game.route.js
+++ enigmathique/backend/src/routes/game.route.js
@@ -34,6 +34,9 @@ module.exports = app => {
 
 
 	// Méthode interne :
+
+	// Y'a moyen de rassembler plusieurs méthodes en une seule
+	// teams + rooms | 
 	
 	// Accepte une équipe au sein de la partie
 	//router.post("/team/accept/:id", middleware.verifyToken, game.accept);
@@ -49,6 +52,9 @@ module.exports = app => {
 
 	// Récupérer l'état d'une à partir de son id
 	router.get("/gameState/:id", middleware.verifyGameToken, game.getState, middleware.verifyErrors);
+
+	// Récupérer la taille max des équipes d'une partie à partir de son id
+	router.get("/maxTeamSize/:id", middleware.verifyGameToken, game.getMaxTeamSize, middleware.verifyErrors);
 
 	// Récupérer les rooms d'une à partir de son id
 	router.get("/rooms/:id", middleware.verifyGameToken, game.getRooms, middleware.verifyErrors);
