d23a479d32c87d5606a9e6f82f98e10e06c5503b
Mathéo <matheo.tardy@etu.univ-grenoble-alpes.fr>
2024-01-23 16:12:18 +0100

    Refactor Team component and TeamContainer component

--- enigmathique/frontend/src/pages/PreGame.jsx
+++ enigmathique/frontend/src/pages/PreGame.jsx
@@ -69,11 +69,11 @@ const PreGame = () => {
 		<SocketContext.Provider value={socket}>
 			<LayoutProf>
 				<main className='flex flex-col gap-4 p-5'>
-				<Notification/>
-				<div className='w-full min-w-[250px] py-2 bg-white primary-font-color text-center text-lg font-semibold shadow-md rounded-full'> Code de connexion : <span className='blue-font-color'>{sessionId}</span> </div>
-				<button className="pregame-join-button" onClick={handleStartGame}>
-					Commencer la partie
-				</button>
+					<Notification/>
+					<div className='w-full min-w-[250px] py-2 bg-white primary-font-color text-center text-lg font-semibold shadow-md rounded-full'> Code de connexion : <span className='blue-font-color'>{sessionId}</span> </div>
+					<button className="pregame-join-button" onClick={handleStartGame}>
+						Commencer la partie
+					</button>
 					<section className="pregame-container">
 						<TeamContainer teams={lockedTeams} isValidated={false} />
 						<TeamContainer teams={confirmedTeams} isValidated={true} />
