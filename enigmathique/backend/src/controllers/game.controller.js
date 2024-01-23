57935fb1ea1506b18bda4d0cfe38239a5ccae850
Sacha Wos <sacha.wos@etu.univ-grenoble-alpes.fr>
2024-01-23 10:22:52 +0100

    feat(api): amélioration commentaire game

--- enigmathique/backend/src/controllers/game.controller.js
+++ enigmathique/backend/src/controllers/game.controller.js
@@ -52,37 +52,15 @@ async function isClassBelongsProfessor(idCourse, req) {
 
 }
 
-// Fonction vérifiant si une classe, à partir de son id, appartiant au professeur
-async function isStudentBelongsProfessor (idStudent, req) {
 
-	// Récupère toutes les classes du professeur courant
-	const student = await Student.findOne({ where: { id: idStudent} });
-
-	// Vérifie que l'élève existe bien
-	if(!student){
-		const error = new Error("L'élève n'existe pas.");
-		error.statusCode = 404;
-		throw error;
-	}
-
-	try{
-		// Vérifie que la classe appartient bien au professeur
-		await isClassBelongsProfessor(student.idCourse, req);
-	} catch(err) {
-		const error = new Error("L'élève n'appartient pas au professeur.");
-		error.statusCode = 403;
-		throw error;
-	}
-
-}
 
 // Fonction vérifiant si une partie, à partir de son id, appartiant au professeur
 async function isGameBelongsProfessor (idGame, req) {
 
-	// Récupère toutes les classes du professeur courant
+	// Récupère toutes les parties du professeur courant
 	const game = await Game.findOne({ where: { id: idGame} });
 
-	// Vérifie que l'élève existe bien
+	// Vérifie que la partie existe bien
 	if(!game){
 		const error = new Error("La partie n'existe pas.");
 		error.statusCode = 404;
@@ -90,8 +68,10 @@ async function isGameBelongsProfessor (idGame, req) {
 	}
 
 	try{
-		// Vérifie que la classe appartient bien au professeur
+		// Vérifie que la classe de la partie appartient bien au professeur
 		await isClassBelongsProfessor(game.idCourse, req);
+
+		// Gère les erreurs
 	} catch(err) {
 		const error = new Error("L'élève n'appartient pas au professeur.");
 		error.statusCode = 403;
@@ -100,34 +80,15 @@ async function isGameBelongsProfessor (idGame, req) {
 
 }
 
-// Fonction vérifiant si une team, à partir de son id, appartiant au professeur
-async function isTeamBelongsProfessor (idTeam, req) {
-
-	// Récupère toutes les teams du professeur courant
-	const team = await Team.findOne({ where: { id: idTeam} });
-
-	// Vérifie que l'élève existe bien
-	if(!team){
-		const error = new Error("L'équipe n'existe pas.");
-		error.statusCode = 404;
-		throw error;
-	}
-
-	try{
-		// Vérifie que la classe appartient bien au professeur
-		await isGameBelongsProfessor(team.idGame, req);
-	} catch(err) {
-		const error = new Error("L'équipe n'appartient pas au professeur.");
-		error.statusCode = 403;
-		throw error;
-	}
-
-}
 
 
 // Fonction vérifiant si la requête est conforme aux attentes
 function isRequestCorrect(schema, req) {
+
+	// Valide le schema avec le contenu de la requête
 	const { error } = schema.validate(req.body);
+
+	// Si le schema n'est pas validé, on lève une erreur
 	if (error) {
 		const validationError = new Error(error.details[0].message);
 		validationError.statusCode = 500;  
@@ -140,7 +101,7 @@ function isRequestCorrect(schema, req) {
 /////////////////////////////////////////////////////////////////////////////////
 
 
-// Créer et enregistrer une nouvelle partie
+// Créer une nouvelle partie
 exports.create = async (req, res, next) => {
 
 	try{
@@ -157,6 +118,7 @@ exports.create = async (req, res, next) => {
 		// Vérifie que la classe appartient bien au professeur
 		await isClassBelongsProfessor(req.body.idCourse, req)
 
+		// Créer une partie à partir des infos fournis
 		const game = {
 			name: req.body.name,
 			idCourse: req.body.idCourse,
@@ -178,10 +140,64 @@ exports.create = async (req, res, next) => {
 	}
 }
 
+//
+// Ajoute des salles à une partie
+exports.addRooms = async(req, res, next) => {
+	
+	try{
+
+		// Vérification des informations fournis
+		const gameSchema = baseSchema.keys({
+			idGame: Joi.number().integer().required(),
+			roomName: Joi.array().items(
+				Joi.string().required()).required()
+		});
+
+		// Vérifie si le schéma correspond bien aux données fournis, renvoie une erreur sinon
+		isRequestCorrect(gameSchema, req)
+
+		// Vérifie que la partie appartient bien au professeur
+		await isGameBelongsProfessor(req.body.idGame, req);
+
+		// Récupère les noms des salles à ajouter
+		const roomNames = req.body.roomName;
+
+		// Map les noms de salles avec l'id de la game
+		const roomsToAdd = roomNames.map(currentRoomName => ({ idGame: req.body.idGame, roomName: currentRoomName }));
+
+		// Enregistrer les rooms dans la table GameRooms
+		const games = GameRooms.bulkCreate(roomsToAdd)
+
+		res.status(201).json(games);
+
+	// Gère les erreurs
+	}catch(err) {
+		next(err)
+	}
+	
+	
+}
+
 
 /////////////////////////////////////////////////////////////////////////////////
 // 									 READ                                      //
 /////////////////////////////////////////////////////////////////////////////////
+
+// methode pour récuperer une classe à partir du code de la partie
+exports.getIdFromCode = async (req, res, next) => {
+
+	try{
+
+		// Récupère la classe courrespondant au code
+		const game = await Game.findOne({ gameCode: { code: req.params.code} })
+
+		return res.status(200).json(game.id);
+
+	// Gère les erreurs
+	}catch(err) {
+		next(err)
+	}	
+}
 
 // Récupère toutes les parties du professeur connecté
 exports.findAll = async (req, res, next) => {
@@ -189,6 +205,7 @@ exports.findAll = async (req, res, next) => {
 
 	try{
 
+		// Stock les IDs des classes du professeur
 		let coursesId = [];
 
 		// Récupère toutes les classes du professeur connecté
@@ -220,11 +237,116 @@ exports.findOne = async (req, res, next) => {
 		// Récupère la partie souhaité
 		const game = await Game.findOne({ where: { id: req.params.id} })
 
-		console.log(game)
-
 		return res.status(200).json(game);
 
 	// Gère les erreurs
+	} catch (err) {
+		next(err)
+	}
+}
+
+// Méthode pour récupérer le score d'une partie
+exports.getScore = async (req, res, next) => {
+
+	try{
+		// Vérifie que la partie appartient bien au professeur
+		await isGameBelongsProfessor(req.params.id, req);
+
+		// Récupère les scores de la partie souhaité
+		const scores = await Score.findAll({ where: { idGame: req.params.id } })
+
+		res.status(200).json(scores);
+
+	}catch(err) {
+		next(err)
+	}
+}
+
+// methode pour vérifier si une partie, à partir de son id, appartient au prof
+exports.gameBelongsToProf = async (req, res, next) => {
+	
+	try{
+
+		// Vérifie que la partie appartient bien au professeur
+		await isGameBelongsProfessor(req.params.id, req);
+
+		// Si on arrive la, c'est qu'il n'y a pas eu d'erreur et donc que la partie appartient au professeur
+		return res.status(200).json({
+			isBelongsTo: true
+		});
+
+	// Gère les erreurs
+	} catch (err) {
+
+		// Si le code d'erreur est 403 cela signifie que la partie n'appartient pas au professeur
+		if(err.statusCode == 403){
+			return res.status(200).json({
+				isBelongsTo: false
+			});
+		}
+
+
+		next(err)
+	}
+}
+
+// methode pour récuperer l'état d'une partie à partir de son id
+exports.getState = async (req, res, next) => {
+	
+	try{
+		// Récupère la partie souhaité
+		const game = await Game.findOne({ where: { id: req.params.id} })
+
+		return res.status(200).json(game.state);
+
+	// Gère les erreurs
+	} catch (err) {
+		next(err)
+	}
+}
+
+// Méthode pour récupérer la taille des équipes à partir de l'id
+exports.getMaxTeamSize = async(req, res, next) => {
+	
+	try{
+		// Récupère la partie souhaité
+		const game = await Game.findOne({ where: { id: req.params.id} })
+		return res.status(200).json(game.teamSize);
+
+	// Gère les erreurs
+	} catch (err) {
+		next(err)
+	}
+}
+
+// Méthode pour récupérer les rooms d'une partie
+exports.getRooms = async (req, res, next) => {
+	
+	try{
+
+		// Récupère la partie souhaité
+		const gameRooms = await GameRooms.findAll({ where: { idGame: req.params.id} })
+
+		// stock les salles de la partie dans rooms
+		const rooms = gameRooms.map(room => room.roomName);
+
+		return res.status(200).json(rooms);
+
+	// Gère les erreurs
+	} catch (err) {
+		next(err)
+	}
+}
+
+// Méthode pour récupèrer les teams d'une partie
+exports.getTeams = async (req, res, next) => {
+	try{
+		// Récupère la partie souhaité
+		const teams = await Team.findAll({ where: { idGame: req.params.id} })
+
+		return res.status(200).json(teams);
+
+		// Gère les erreurs
 	} catch (err) {
 		next(err)
 	}
@@ -234,7 +356,7 @@ exports.findOne = async (req, res, next) => {
 // 									 UPDATE                                    //
 /////////////////////////////////////////////////////////////////////////////////
 
-// methode pour mettre à jour le professeur connecté
+// methode pour mettre à jour le l'état de la partie. 0 : en cours de création, 1 : en cours, 1 : terminé
 exports.setState = async(req, res, next) => {
 	
 	try{
@@ -266,56 +388,7 @@ exports.setState = async(req, res, next) => {
 	}
   };
 
-
-/////////////////////////////////////////////////////////////////////////////////
-// 									 OTHER                                     //
-/////////////////////////////////////////////////////////////////////////////////
-
-exports.getScore = async (req, res, next) => {
-
-	try{
-		// Vérifie que la partie appartient bien au professeur
-		await isGameBelongsProfessor(req.params.id, req);
-
-		// Récupère les scores de la partie souhaité
-		const scores = await Score.findAll({ where: { idGame: req.params.id } })
-
-		res.status(200).json(scores);
-
-	}catch(err) {
-		next(err)
-	}
-}
-
-// methode pour vérifier si une partie, à partir de son id, appartient au prof
-exports.gameBelongsToProf = async (req, res, next) => {
-	
-	try{
-
-		// Vérifie que la partie appartient bien au professeur
-		await isGameBelongsProfessor(req.params.id, req);
-
-		return res.status(200).json({
-			isBelongsTo: true
-		});
-
-	// Gère les erreurs
-	} catch (err) {
-
-		// Si le code d'erreur est 403 cela signifie que la partie n'appartient pas au professeur
-		if(err.statusCode == 403){
-			return res.status(200).json({
-				isBelongsTo: false
-			});
-		}
-
-
-		next(err)
-	}
-}
-
-
-// Ouvre la partie aux élèves
+// Méthode pour "ouvrir" la partie aux élèves, cela génère un Code qui permet aux élèves de rejoindre la partie
 exports.open = async (req, res, next) => {
 
 	try{
@@ -323,18 +396,20 @@ exports.open = async (req, res, next) => {
 		// Vérifie que la partie appartient bien au professeur
 		await isGameBelongsProfessor(req.params.id, req)
 
-		// Récupère l'id de la classe de la partie
+		// Récupère la partie
 		const game =  await Game.findOne({ where: { id: req.params.id} })
 
+		// Vérifie que la partie ne soit pas déjà ouverte où terminé
 		if(game.gameCode || game.state == 2){
 			const validationError = new Error("La partie est déjà ouverte ou terminé.");
 			validationError.statusCode = 500;
 			throw validationError;
 		}
 
+		// Génère un code de 10 caractère
 		const code = makeid(10);
 
-		// Définit le gameCode de la partie avec une chaîne de caractère aléatoire
+		// Attribut le gameCode à la partie
 		const updatedRows = await Game.update({gameCode: code},{where: { id: req.params.id }});
 		
 		// Vérifie que la colonne à effectivement été mise à jour
@@ -344,132 +419,13 @@ exports.open = async (req, res, next) => {
 			throw error;
 		}
 
+		// Retourne le code
 		return res.status(201).json({gameCode: code});
 
 		// Gère les erreurs
 	}catch(err) {
 		next(err)
 	}
-}
-
-// todo : le dete renvoie des données ?
-// Ferme la partie aux élèves
-exports.close = async (req, res, next) => {
-
-	try{
-		// Vérifie que la partie appartient bien au professeur
-		await isGameBelongsProfessor(req.params.id, req);
-
-		// Enregistrer la classe dans la base de données
-		const destroyedGameCode = await GameCode.destroy({ where: { idGame: req.params.id}})
-
-		// Renvoie les données supprimées
-		const updatedRows = await Game.update({state: 1},{where: { id: req.params.id }});
-		return res.status(200).json(updatedRows);
-		
-
-	// Gère les erreurs
-	}catch(err) {
-		next(err)
-	}
-}
-
-// Ferme la partie aux élèves
-exports.delete = async (req, res, next) => {
-
-	try{
-		// Vérifie que la partie appartient bien au professeur
-		await isGameBelongsProfessor(req.params.id, req);
-
-		// Enregistrer la classe dans la base de données
-		const destroyedRows = await Game.destroy({ where: { id: req.params.id}})
-		return res.status(200).json(destroyedRows);
-		
-
-	// Gère les erreurs
-	}catch(err) {
-		next(err)
-	}
-}
-
-// Supprime la partie
-exports.backendDelete = async (req, res, next) => {
-
-	try{
-		// Enregistrer la classe dans la base de données
-		const destroyedRows = await Game.destroy({ where: { id: req.params.id}})
-		return res.status(200).json(destroyedRows);
-		
-
-	// Gère les erreurs
-	}catch(err) {
-		next(err)
-	}
-}
-
-// Termine la partie
-exports.end = async (req, res, next) => {
-
-	try{
-		// La partie s'est terminée normalement ?
-		const endedNormally = req.body.endedNormally;
-
-		if (endedNormally == true) {
-			// Enregistrer la classe dans la base de données
-			const updatedRows = await Game.update({state: 2,gameCode: null},{where: { id: req.params.id }})
-
-			// Renvoie les données mise a jour
-			return res.status(201).json(updatedRows);
-		} else {
-			// Supprimer la partie
-			const destroyedRows = await Game.destroy({ where: { id: req.params.id }});
-			// Renvoie les données supprimées
-			return res.status(201).json(destroyedRows);
-		}
-
-
-	// Gère les erreurs
-	}catch(err) {
-		console.log(err)
-		next(err)
-	}
-}
-
-
-
-// methode pour récuperer une classe à partir du code de la partie
-exports.getIdFromCode = async (req, res, next) => {
-
-	try{
-
-		// Récupère la classe courrespondant au code
-		const game = await Game.findOne({ gameCode: { code: req.params.code} })
-
-		return res.status(200).json(game.id);
-
-	// Gère les erreurs
-	}catch(err) {
-		next(err)
-	}	
-}
-
-// methode pour récuperer une classe à partir du code de la partie
-exports.course = async (req, res, next) => {
-
-	try{
-		// Récupère la classe correspondant au code
-		const gameCode = await Game.findOne({ where: { id: req.params.id} })
-		// Récupèrer les élèves de la classe
-		const students = await Student.findAll({ where: { idCourse: gameCode.idCourse} })
-
-		// Renvoie les données récupérées
-		return res.status(200).json(students);
-
-	// Gère les erreurs
-	}catch(err) {
-		console.log(err)
-		next(err)
-	}	
 }
 
 //
@@ -491,12 +447,15 @@ exports.addRooms = async(req, res, next) => {
 		// Vérifie que la partie appartient bien au professeur
 		await isGameBelongsProfessor(req.body.idGame, req);
 
-		// Récupère les id des salles à ajouter
+		// Récupère les noms des salles à ajouter
 		const roomNames = req.body.roomName;
+
+		// Map les noms de salles avec l'id de la game
 		const roomsToAdd = roomNames.map(currentRoomName => ({ idGame: req.body.idGame, roomName: currentRoomName }));
 
 		// Enregistrer les rooms dans la table GameRooms
 		const games = GameRooms.bulkCreate(roomsToAdd)
+
 		res.status(201).json(games);
 
 	// Gère les erreurs
@@ -507,56 +466,89 @@ exports.addRooms = async(req, res, next) => {
 	
 }
 
+/////////////////////////////////////////////////////////////////////////////////
+// 									 DELETE                                    //
+/////////////////////////////////////////////////////////////////////////////////
 
-// methode pour récuperer une partie en fonction de son id
-exports.getState = async (req, res, next) => {
-	
+// Supprime une partie
+exports.delete = async (req, res, next) => {
+
 	try{
-		// Récupère la partie souhaité
-		const game = await Game.findOne({ where: { id: req.params.id} })
-		return res.status(200).json(game.state);
+		// Vérifie que la partie appartient bien au professeur
+		await isGameBelongsProfessor(req.params.id, req);
+
+		// Supprime la partie correspondante à l'ID
+		const deletedRows = await Game.destroy({ where: { id: req.params.id}})
+
+		// Vérifie que la colonne à effectivement été supprimé
+		if (deletedRows == 0) {
+			const error = new Error("Impossible de supprimer la partie.");
+			error.statusCode = 404;
+			throw error;
+		}
+
+		return res.status(201).json("Partie supprimé avec succès");		
 
 	// Gère les erreurs
-	} catch (err) {
+	}catch(err) {
 		next(err)
 	}
 }
 
-exports.getMaxTeamSize = async(req, res, next) => {
-	
+// Supprime la partie
+exports.backendDelete = async (req, res, next) => {
+
 	try{
-		// Récupère la partie souhaité
-		const game = await Game.findOne({ where: { id: req.params.id} })
-		return res.status(200).json(game.teamSize);
+
+		// Ici pas besoins de vérifier l'appartenance de la partie, car il s'agit d'une méthode réservé pour la backend
+
+		// Enregistrer la classe dans la base de données
+		const destroyedRows = await Game.destroy({ where: { id: req.params.id}})
+
+		// Vérifie que la colonne à effectivement été supprimé
+		if (deletedRows == 0) {
+			const error = new Error("Impossible de supprimer la partie.");
+			error.statusCode = 404;
+			throw error;
+		}
+
+		return res.status(201).json("Partie supprimé avec succès");	
+		
 
 	// Gère les erreurs
-	} catch (err) {
+	}catch(err) {
 		next(err)
 	}
 }
 
-exports.getRooms = async (req, res, next) => {
-	
-	try{
 
-		// Récupère la partie souhaité
-		const gameRooms = await GameRooms.findAll({ where: { idGame: req.params.id} })
-		// Met dans un tableau les noms des salles
-		const rooms = gameRooms.map(room => room.roomName);
-		return res.status(200).json(rooms);
+
+
+
+
+
+
+
+
+
+
+// methode pour récuperer une classe à partir du code de la partie
+exports.course = async (req, res, next) => {
+
+	try{
+		// Récupère la classe correspondant au code
+		const gameCode = await Game.findOne({ where: { id: req.params.id} })
+		// Récupèrer les élèves de la classe
+		const students = await Student.findAll({ where: { idCourse: gameCode.idCourse} })
+
+		// Renvoie les données récupérées
+		return res.status(200).json(students);
+
 	// Gère les erreurs
-	} catch (err) {
+	}catch(err) {
+		console.log(err)
 		next(err)
-	}
+	}	
 }
 
-exports.getTeams = async (req, res, next) => {
-	try{
-		// Récupère la partie souhaité
-		const teams = await Team.findAll({ where: { idGame: req.params.id} })
-		return res.status(200).json(teams);
-		// Gère les erreurs
-	} catch (err) {
-		next(err)
-	}
-}
+
