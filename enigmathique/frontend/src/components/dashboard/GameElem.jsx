77f1085d0995898c8d18869190000435d413a336
Raph255 <raphael.pivot2505@gmail.com>
2024-01-23 23:15:51 +0100

    feat (web) : refactor de l'affichage des parties (Mes parties) - changement des boutons et  liens fonctionnels selon l'état de la partie

--- enigmathique/frontend/src/components/dashboard/GameElem.jsx
+++ enigmathique/frontend/src/components/dashboard/GameElem.jsx
@@ -1,11 +1,8 @@
 import {Link} from 'react-router-dom';
 import React, {useEffect, useState, useTransition} from 'react';
 import PropTypes from 'prop-types';
-import TeamService from "../../services/team.service";
+
 import GameModel from "../../models/game.model";
-import CourseModel from "../../models/course.model";
-import useCourses from "../../hooks/useCourses";
-import useTeams from "../../hooks/useTeams";
 import TeamModel from "../../models/team.model";
 import useCourse from "../../hooks/useCourse";
 import InfoBlockElem from "./InfoBlockElem";
@@ -104,6 +101,7 @@ const GameElem = (props) => {
 	let winnerLabel ;
 	let successRateLabel ;
 	let link ;
+	console.log(game.state);
 	switch (game.state) {
 		case 0:
 			winnerLabel = "Partie non lancée" ;
--- enigmathique/frontend/src/pages/Games.jsx
+++ enigmathique/frontend/src/pages/Games.jsx
@@ -11,6 +11,7 @@ import ActionButton from "components/dashboard/ActionButton";
 import Modal, {ModalBody, ModalHeader} from "../components/Modal";
 import toast from "react-hot-toast";
 import Notification from "components/Notification";
+import GameRow from "components/dashboard/GameRow";
 
 import {Link} from "react-router-dom";
 
@@ -18,7 +19,7 @@ const Games = () => {
 	const [games, setGames] = useState([]);
 	const [filter, setFilter] = useState('');
 	const [filteredGames, setFilteredGames] = useState([]);
-	const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+
 	const loadGames = async () => {
 	//récupérer toutes les games
 	const data = await GameModel.getAll();
@@ -38,20 +39,6 @@ const Games = () => {
 	setGames(updateGames);
 	}
 
-	const handleClickDelete = async (event, id) => {
-		event.preventDefault();
-		await toast.promise(
-			GameModel.delete(id),
-			{
-				loading: 'Suppression...',
-				success: "La partie a bien été supprimée",
-				error: "Une erreur s'est produite",
-			}
-		);
-		loadGames();
-		setDeleteModalOpen(false);
-	}
-
 	useEffect(() => {
 		loadGames();
 	}, []);
@@ -64,6 +51,8 @@ const Games = () => {
 		);
 		setFilteredGames([...filtered]);
 	}, [filter, games]);
+
+
 	return (
 		<LayoutProf>
 			<main>
@@ -72,54 +61,14 @@ const Games = () => {
 					<SearchInput handleChangeText={handleTextChange}/>
 					<Link to='/create-game'> <CreateButton title="Créer une partie" onClick={() => null}/> </Link>
 				</ContentHeader>
-				<TableContainer headers={['Nom', 'Date', 'Score', 'Taux de réussite', 'Nombre de salles', 'Action']}>
-					{filteredGames.map((game, index) => {							return (
-								<tr key={index} className={`border-t border-[#CECDFD] ${index % 2 == 0 ? 'bg-[#EBECF9]' : 'bg-[#F1F3FA]'}`}>
-									<td className="pl-5 td-style">
-										{game.name}
-									</td>
-									<td className="td-style">{game.createdAt.toLocaleDateString('fr-FR')}</td>
-									<td className="td-style">--</td>
-									<td className="td-style">--%</td>
-									<td className="td-style">--</td>
-									<td className="td-style text-right pr-5">
-										<div className="space-x-3">
-										<ActionButton 
-											title="Classement"
-											link={game.state === 2 ? `/ranking/${game.id}` : game.state === 0 ? `/pregame/${game.gameCode}` : `/leaderboard?idSession=${game.gameCode}`}
-										/>
-										<ActionButton
-											title="Détails"
-											link={`/game/${game.id}`}
-											disabled={game.state !== 2}
-										/>
-										<ActionButton
-											title="Supprimer"
-											onClick={() => setDeleteModalOpen(true)}
-										/>
-										</div>
-									</td>
-									{deleteModalOpen && (
-									<Modal setOpenModal={setDeleteModalOpen}>
-										<ModalHeader title="Supprimer une partie"/>
-										<ModalBody>
-											<form className='flex flex-col text-center w-full gap-3'>
-												<p className=' block text-sm font-medium mb-5 primary-font-color'>Êtes-vous sûr de vouloir supprimer la partie {game.name} ?</p>
-												<button
-													type='submit'
-													className='modal-validate-button-style bg-[#ef4565] hover:bg-red-500'
-													onClick={(event) => handleClickDelete(event, game.id)}>
-													Supprimer
-												</button>
-												<button
-													className='modal-cancel-button-style'
-													onClick={() => setDeleteModalOpen(false)}>
-													Annuler
-												</button>
-											</form>
-										</ModalBody>
-									</Modal>)}
-								</tr>
+				<TableContainer headers={['Nom', 'Date', 'Classe', 'Taux de réussite', 'Action']}>
+					{filteredGames.map((game, index) => {							
+						return (
+							<GameRow
+								key={index}
+								game={game}
+								loadGames={loadGames}
+							/>	
 							);
 						})}
 				</TableContainer>
