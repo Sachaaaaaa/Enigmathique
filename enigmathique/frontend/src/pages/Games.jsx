a8ddaa82bc84539d08b773a70ae9b6e4c7cddc3c
Yvain Dalban <yvain.dalban@etu.univ-grenoble-alpes.fr>
2024-01-23 10:54:02 +0100

    fix (web): création component GameElement dans Games pour régler le problème de suppression

--- enigmathique/frontend/src/pages/Games.jsx
+++ enigmathique/frontend/src/pages/Games.jsx
@@ -7,18 +7,18 @@ import SearchInput from "../components/SearchInput";
 import CreateButton from "components/dashboard/CreateButton";
 import ContentHeader from "components/dashboard/ContentHeader";
 import TableContainer from "components/dashboard/TableContainer";
-import ActionButton from "components/dashboard/ActionButton";
-import Modal, {ModalBody, ModalHeader} from "../components/Modal";
 import toast from "react-hot-toast";
 import Notification from "components/Notification";
+import GameElement from "../components/gameList/GameElement";
 
 import {Link} from "react-router-dom";
 
 const Games = () => {
+
 	const [games, setGames] = useState([]);
 	const [filter, setFilter] = useState('');
 	const [filteredGames, setFilteredGames] = useState([]);
-	const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+
 	const loadGames = async () => {
 	//récupérer toutes les games
 	const data = await Game.getAll();
@@ -38,32 +38,21 @@ const Games = () => {
 	setGames(updateGames);
 	}
 
-	const handleClickDelete = async (event, id) => {
-		event.preventDefault();
-		await toast.promise(
-			Game.delete(id),
-			{
-				loading: 'Suppression...',
-				success: "La partie a bien été supprimée",
-				error: "Une erreur s'est produite",
-			}
-		);
-		loadGames();
-		setDeleteModalOpen(false);
-	}
-
 	useEffect(() => {
 		loadGames();
 	}, []);
+
 	const handleTextChange = (e) => {
 		setFilter(e.target.value);
 	};
+
 	useEffect(() => {
 		const filtered = games.filter(
 			(game) => game.name.toLowerCase().includes(filter.toLowerCase())
 		);
 		setFilteredGames([...filtered]);
 	}, [filter, games]);
+
 	return (
 		<LayoutProf>
 			<main>
@@ -73,57 +62,10 @@ const Games = () => {
 					<Link to='/create-game'> <CreateButton title="Créer une partie" onClick={() => null}/> </Link>
 				</ContentHeader>
 				<TableContainer headers={['Nom', 'Date', 'Score', 'Taux de réussite', 'Nombre de salles', 'Action']}>
-					{filteredGames.map((game, index) => {							return (
-								<tr key={index} className={`border-t border-[#CECDFD] ${index % 2 == 0 ? 'bg-[#EBECF9]' : 'bg-[#F1F3FA]'}`}>
-									<td className="pl-5 td-style">
-										{game.name}
-									</td>
-									<td className="td-style">{game.createdAt.toLocaleDateString('fr-FR')}</td>
-									<td className="td-style">--</td>
-									<td className="td-style">--%</td>
-									<td className="td-style">--</td>
-									<td className="td-style text-right pr-5">
-										<div className="space-x-3">
-										<ActionButton 
-											title="Classement"
-											link={game.state === 2 ? `/ranking/${game.id}` : game.state === 0 ? `/pregame/${game.gameCode}` : `/leaderboard?idSession=${game.gameCode}`}
-										/>
-										<ActionButton
-											title="Détails"
-											link={`/game/${game.id}`}
-											disabled={game.state !== 2}
-										/>
-										<ActionButton
-											title="Supprimer"
-											onClick={() => setDeleteModalOpen(true)}
-										/>
-										</div>
-									</td>
-									{deleteModalOpen && (
-									<Modal setOpenModal={setDeleteModalOpen}>
-										<ModalHeader title="Supprimer une partie"/>
-										<ModalBody>
-											<form className='flex flex-col text-center w-full gap-3'>
-												<p className=' block text-sm font-medium mb-5 primary-font-color'>Êtes-vous sûr de vouloir supprimer la partie {game.name} ?</p>
-												<button
-													type='submit'
-													className='modal-validate-button-style bg-[#ef4565] hover:bg-red-500'
-													onClick={(event) => handleClickDelete(event, game.id)}>
-													Supprimer
-												</button>
-												<button
-													className='modal-cancel-button-style'
-													onClick={() => setDeleteModalOpen(false)}>
-													Annuler
-												</button>
-											</form>
-										</ModalBody>
-									</Modal>)}
-								</tr>
-							);
-						})}
+					{filteredGames.map((game, index) =>
+						<GameElement key={index} game={game} index={index} onChange={() => loadGames()}/>
+					)}
 				</TableContainer>
-
 			</main>
 		</LayoutProf>
 	);
