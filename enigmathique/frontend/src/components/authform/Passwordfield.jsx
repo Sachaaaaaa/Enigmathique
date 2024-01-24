1a926e54d200976cedb16b1df1cbe1b5e7943048
Matheo.G <matheoguillevic@gmail.com>
2024-01-24 23:24:04 +0100

    feat(web): gestion des erreurs lors de l'inscription

--- enigmathique/frontend/src/components/authform/Passwordfield.jsx
+++ enigmathique/frontend/src/components/authform/Passwordfield.jsx
@@ -1,43 +1,44 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import { IoEye, IoEyeOff } from 'react-icons/io5';
+import {IoEye, IoEyeOff} from 'react-icons/io5';
 
-const Passwordfield = ({ label, name, value, onChange }) => {
-    const [showPassword, setShowPassword] = useState(false);
-    return (
-        <div className='mb-4 w-full'>
-            <label className='form-label-style' htmlFor={name}>
-                {label}
-            </label>
-            <div className='relative'>
-                <input
-                    type={showPassword ? 'text' : 'password'}
-                    id={name}
-                    name={name}
-                    value={value}
-                    onChange={onChange}
-                    placeholder='Votre mot de passe'
-                    className='form-inputfield-style'
-                    required
-                />
-                <button
-                    className='absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5'
-                    onMouseDown={() => setShowPassword(true)}
-                    onMouseUp={() => setShowPassword(false)}
-                    type="button"
-                >
-                    {showPassword ? <IoEye size={20}/> : <IoEyeOff size={20} />}
-                </button>
-            </div>
-        </div>
-    );
+const Passwordfield = ({label, name, value, onChange, style}) => {
+	const [showPassword, setShowPassword] = useState(false);
+	return (
+		<div className='mb-4 w-full'>
+			<label className='form-label-style' htmlFor={name}>
+				{label}
+			</label>
+			<div className='relative'>
+				<input
+					type={showPassword ? 'text' : 'password'}
+					id={name}
+					name={name}
+					value={value}
+					onChange={onChange}
+					placeholder='Votre mot de passe'
+					className={`form-inputfield-style ${style}`}
+					required
+				/>
+				<button
+					className='absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5'
+					onMouseDown={() => setShowPassword(true)}
+					onMouseUp={() => setShowPassword(false)}
+					type="button"
+				>
+					{showPassword ? <IoEye size={20}/> : <IoEyeOff size={20}/>}
+				</button>
+			</div>
+		</div>
+	);
 }
 
 Passwordfield.propTypes = {
-    label: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
+	label: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	value: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
+	style: PropTypes.string,
 };
 
 export default Passwordfield;
