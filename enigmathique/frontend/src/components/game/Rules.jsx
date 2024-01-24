d94056c0a72c236d0f5d342c07dad95a0f5aea70
Raph255 <raphael.pivot2505@gmail.com>
2024-01-24 22:33:48 +0100

    style(game): rules pop-up fini

--- enigmathique/frontend/src/components/game/Rules.jsx
+++ enigmathique/frontend/src/components/game/Rules.jsx
@@ -10,6 +10,7 @@ import { HiCursorClick } from "react-icons/hi"
 import leftclick from "../../assets/img/mouse-img/clicgauche.png";
 import rightclick from "../../assets/img/mouse-img/clicdroit.png";
 import scroll from "../../assets/img/mouse-img/scroll.png";
+import './game.css'
 import { IconContext } from "react-icons";  
 
 
@@ -17,7 +18,6 @@ const articleClassname = "flex flex-col gap-2 w-full" ;
 const titleDivClassname = "font-semibold primary-font-color py-3 uppercase" ;
 const ruleDivClassname = "flex justify-left items-center gap-2 w-full primary-font-color" ;
 
-const iconDivClassname = "w-fit p-2"
 const Rules = ({onCloseClick}) => {
     return (
 
@@ -30,70 +30,67 @@ const Rules = ({onCloseClick}) => {
                 }}
             >
             <section className='flex gap-3'>
-                <article className={articleClassname}>
-                    <h2 className={titleDivClassname}> Déplacements</h2>
-                    <div className={ruleDivClassname}>
+                <article className="articleClassname">
+                    <h2 className="titleDivClassname"> Déplacements</h2>
+                    <div className="ruleDivClassname">
                         <img src={leftclick} alt="leftclick" width={40}/>
-                        <p> Tournez la salle en maintenant clic gauche</p>
+                        <p> <strong>Tournez</strong> la salle en maintenant clic gauche</p>
                     </div>
-                    <div className={ruleDivClassname}>
+                    <div className="ruleDivClassname">
                         <img src={rightclick} alt="rightclick" width={40} />
-                        <p> Déplacez la salle grâce au clic droit</p>               
+                        <p> <strong>Déplacez</strong> la salle grâce au clic droit</p>               
                     </div>
-                    <div className={ruleDivClassname}>
+                    <div className="ruleDivClassname">
                         <img src={scroll} alt="scroll" width={40}/>
-                        <p> Zoomez avec la molette </p>               
+                        <p> <strong>Zoomez</strong> avec la molette </p>               
                     </div>
                 </article>
 
-                <article className={articleClassname}>
-                    <h2 className={titleDivClassname}> Objectif </h2>
-                    <div className={ruleDivClassname}>
-                        <div className={iconDivClassname}>
+                <article className="articleClassname">
+                    <h2 className="titleDivClassname"> Objectif </h2>
+                    <div className="ruleDivClassname">
+                        <div className="iconDivClassname">
                             <BsFillDoorOpenFill size='2em' />
                         </div>
-                        <p> Votre objectif ? Vous échappez de la pièce dans le temps imparti ! Dans chaque salle, cliquez sur la porte pour afficher le contexte.</p>
+                        <p> Votre objectif ? Vous <strong>échappez</strong> de la pièce dans le <strong>temps imparti</strong> ! Dans chaque salle, cliquez sur la <strong>porte</strong> pour afficher le contexte.</p>
                     </div>
-                    <div className={ruleDivClassname}>
-                        <div className={iconDivClassname}>
+                    <div className="ruleDivClassname">
+                        <div className="iconDivClassname">
                             <IoMdTrophy  size='2em' />
                         </div>
                         <div>
-                            <p> <strong></strong> +500 points si vous sortez de la salle</p>
-                            <p> +100 points par énigme résolue</p>
-                            <p> -10 points par indice utilisé</p>
-                            <p> -20 points par mauvaise réponse</p>
+                            <p> <span className='green-font-color font-medium' > +500 points </span> si vous sortez de la salle</p>
+                            <p> <span className='green-font-color font-medium'> +100 points </span> par énigme résolue</p>
+                            <p> <span className='red-font-color font-medium'>-10 points</span> par indice utilisé</p>
+                            <p> <span className='red-font-color font-medium'> -20 points</span> par mauvaise réponse</p>
                         </div>
                     </div>
                 </article>
                 
-                <article className={articleClassname}>
-                    <h2 className={titleDivClassname}> Enigmes </h2>
-                    <div className={ruleDivClassname}>
-                        <div className={iconDivClassname}>
+                <article className="articleClassname">
+                    <h2 className="titleDivClassname"> énigmes </h2>
+                    <div className="ruleDivClassname">
+                        <div className="iconDivClassname">
                             <IoIosSearch  size='2em' />
                         </div>
-                        <p> Passez votre souris sur tous les éléments de la scène pour voir lesquels sont interactifs. Ceux-ci <strong> changent de couleur </strong>.
+                        <p> Passez votre souris sur tous les <strong>éléments</strong> de la scène pour voir lesquels sont <strong>interactifs</strong>. Ceux-ci <strong> changent de couleur </strong>.
                             Certains éléments sont durs à trouver, ouvrez {"l'oeil !"} </p>
                     </div>
-                    <div className={ruleDivClassname}>
-                        <div className={iconDivClassname}>
+                    <div className="ruleDivClassname">
+                        <div className="iconDivClassname">
                             <HiCursorClick  size='2em' />
                         </div>
                         <p>
-                            Pour fermer une énigme ou une autre fenêtre vous pouvez appuyer sur 
-                            la <strong>croix</strong> en bas ou <strong>rappuyer</strong> sur l&apos;objet cliqué.
+                            Pour <strong>fermer</strong> une énigme ou une autre fenêtre vous pouvez appuyer sur 
+                            la <strong>croix</strong> rouge ou <strong>rappuyer</strong> sur {"'objet"} cliqué.
                         </p>
                     </div>
                 </article>
             </section>
             </IconContext.Provider>
-
-
             <div className='w-full flex justify-end'>
-            <button className='
-													flex justify-center items-center
-								btn-action text-[#EF4565] p-0'
+            <button className=' flex justify-center items-center
+								btn-action red-font-color p-0'
 								onClick={onCloseClick}><MdCancel size='4em'/></button>
             </div>
 		</div>
