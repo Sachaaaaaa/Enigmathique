4360b4e9ecb1d5ee6ea7314454a324d3393bd6fc
Mathéo <matheo.tardy@etu.univ-grenoble-alpes.fr>
2024-01-25 10:36:11 +0100

    fix(web): Redirection si la classe n'existe pas

--- enigmathique/frontend/src/pages/Students.jsx
+++ enigmathique/frontend/src/pages/Students.jsx
@@ -1,16 +1,35 @@
 import LayoutProf from '../layouts/LayoutProf';
-import React from 'react';
+import React, {useEffect} from 'react';
 import ListStudents from '../components/student/ListStudents';
 import Notification from '../components/Notification';
-import {useParams} from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
+import StudentModel from "../models/student.model";
 
 function Students() {
-	const {id} = useParams()
+	const {id} = useParams();
+	const [students, setStudents] = React.useState([]);
+	const navigate = useNavigate();
+	const loadStudents = async () => {
+
+		const data = await StudentModel.getAll(id);
+		console.log(data);
+		setStudents(data);
+
+		if (data !== undefined) {
+			return;
+		}
+		navigate('/class');
+	};
+	useEffect(() => {
+		loadStudents();
+	}, []);
+
 	return (
 		<LayoutProf>
 			<main>
 				<Notification/>
-				<ListStudents id={parseInt(id)}/>
+				<ListStudents students={students} id={id}/>
+
 			</main>
 		</LayoutProf>
 	);
