4822b93678fca3f2c280f164813a8f2ff86a5afb
Thibaut DUPUIS <thibaut.dupuis@etu.univ-grenoble-alpes.fr>
2024-01-25 14:48:21 +0100

    fix(web): graph winrate

--- enigmathique/frontend/src/components/dashboard/ClassElem.jsx
+++ enigmathique/frontend/src/components/dashboard/ClassElem.jsx
@@ -5,74 +5,71 @@ import {Link} from 'react-router-dom';
 import { Chart as ChartJS, registerables } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import GameService from '../../services/game.service';
-import StudentModel from '../../models/student.model';
-import GameModel from "../../models/game.model";
+import StudentModel from 'models/student.model';
+import GameModel from 'models/game.model';
 import InfoBlockElem from './InfoBlockElem';
+import ScoreModel from 'models/score.model';
 ChartJS.register(...registerables);
 
 const maxTime = 600;
 
-const ClassElem = (props) => {
+const ClassElem = ({classGroup}) => {
 
-	const classGroup = props.classGroup;
-
-	const [gamesOf, setGamesOf] = useState([]);
 	const [students, setStudents] = useState([]);
+	const [gamesOfClass, setGamesOfClass] = useState([]);
+	const [winrate, setWinrate] = useState(0);
+	
+	const loadData = async() => {
+		// Chargement les élèves
+		const students = await StudentModel.getAll(classGroup.id);
+		setStudents(students);
 
-	const loadStudents = async () => {
-		const data = await StudentModel.getAll(classGroup.id);
-		setStudents(data);
-	}
-
-	useEffect(() => {
-		loadStudents();
-	}, []);
-
-	const loadGamesOf = async() => {
-		const data = await GameModel.getAll();
+		// Chargement des parties
+		const games = await GameModel.getAll();
+		
+		// Filtre les parties => celles de la classe
 		let listGames = [];
-		data.forEach((game) => {
-			classGroup.id === game.idCourse && listGames.push();
+		games.forEach((game) => {
+			classGroup.id === game.idCourse && listGames.push(game);
 		});
-		setGamesOf(listGames);
-	}
+		setGamesOfClass(listGames);
+
+		const getGameData = async(game) => {
+			// Chargement des scores
+			const scores = await GameModel.getScores(game.id);
+			// Filtre les scores => ceux de la classe
+			let totalSolved = 0;
+			let totalRoom = 0;
+			scores.forEach((score) => {
+				score.isSolved && totalSolved++; // mdr
+				totalRoom++;
+			});
+			return [totalSolved, totalRoom];
+		};
+
+		const calculateWinrate = async() => {
+			// getGameData pour toutes les parties
+			let totalSolved = 0;
+			let totalRoom = 0;
+			for (let i = 0; i < listGames.length; i++) {
+				const game = listGames[i];
+				const [solved, room] = await getGameData(game);
+				totalSolved += solved;
+				totalRoom += room;
+			}
+
+			// Calcul du taux de réussite
+			const winrate = Math.round((totalSolved / totalRoom) * 100);
+			setWinrate(winrate);
+		};
+	
+
+		const winrate = await calculateWinrate();
+	};
 
 	useEffect(() => {
-		loadGamesOf();
+		loadData();
 	}, []);
-
-	const getLastGame = (games) => {
-		if(games.length !== 0) {
-			let maxDate = games[0].createdAt;
-			games.forEach((game) => {
-				(game.createdAt.localeCompare(maxDate) > 0) && (maxDate = game.createdAt);
-			});
-			return maxDate;
-		} else {
-			return 'jamais joué'
-		}
-	}
-
-	const getWinRate = (games) => {
-		let winRate = 0;
-		let nbScore;
-		if(games.length !== 0) {
-			games.forEach((game) => {
-				GameService.getScores(game.id).then((response) => {
-					const scoreList = response;
-					scoreList.forEach((score) => {
-						score.time < maxTime && winRate++;
-					});
-					nbScore = scoreList.length;
-				}).catch((error) => {
-					console.log(error);
-				});
-			});
-			return Math.floor((winRate / nbScore) * 100);
-		} else {
-			return 0;
-		}
-	}
 
 
 	/*Données de génération du graphique*/
@@ -81,11 +78,11 @@ const ClassElem = (props) => {
 		datasets: [
 			{
 				label: 'Taux de réussite',
-				data: [getWinRate(gamesOf)],
+				data: [winrate],
 				backgroundColor: 'green',
 			}
 		],
-	}
+	};
 
 	const options = {
 		barPercentage: 1,
@@ -102,7 +99,7 @@ const ClassElem = (props) => {
 		},
 		indexAxis: 'y',
 		borderRadius: 20
-	}
+	};
 
 
 	return (
@@ -114,15 +111,15 @@ const ClassElem = (props) => {
 
 			<InfoBlockElem title='élèves' text={students.length}/>
 
-			<InfoBlockElem title='nombre de parties jouées' text={gamesOf.length}/>
+			<InfoBlockElem title='nombre de parties jouées' text={gamesOfClass.length}/>
 
-			<InfoBlockElem title='dernière partie' text={getLastGame(gamesOf)}/>
+			<InfoBlockElem title='dernière partie' text={'getLastGame(gamesOfClass)'}/>
 
 			{/* Div pour les statistiques */}
 			<Bar className='col-span-2 row-span-2' data={data} options={options}/>
 			<div className='col-span-2'>
 				<p className='text-sm font-semibold'>Taux de
-					réussite moyen : {getWinRate(gamesOf)}%</p>
+					réussite moyen : {winrate}%</p>
 			</div>
 
 			{/* Bouton 'Voir' */}
@@ -130,11 +127,11 @@ const ClassElem = (props) => {
 				Voir
 			</Link>
 		</div>
-	)
-}
+	);
+};
 
 ClassElem.propTypes = {
 	classGroup: PropTypes.object.isRequired
-}
+};
 
 export default ClassElem;
