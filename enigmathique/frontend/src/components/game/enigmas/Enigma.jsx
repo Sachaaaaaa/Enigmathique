169a243f9de22a8e44786e717d45a0d44a86ddbd
Raph255 <raphael.pivot2505@gmail.com>
2024-01-25 18:39:32 +0100

    style(web) : avancement design game pop-up

--- enigmathique/frontend/src/components/game/enigmas/Enigma.jsx
+++ enigmathique/frontend/src/components/game/enigmas/Enigma.jsx
@@ -84,7 +84,7 @@ const Enigma = ({ enigmaId, enigmaDisplayTemplate, closeEnigma, title="" }) => {
 
 				<div className='p-3 pt-0'>
 				{enigmaDisplayTemplate(variables, enigmaState.hint, enigmaState.isSolved, submitAnswer, askHint)}
-				<div className='text-green-600'>
+				<div className='m-1.5 text-green-600'>
 					{enigmaState.isSolved && <p>{enigmaState.endMessage}</p>}
 				</div>
 				</div>
@@ -102,108 +102,3 @@ Enigma.propTypes = {
 	closeEnigma: PropTypes.func.isRequired,
 	title: PropTypes.string,
 };
-
-/*
-
-const Enigme = ({ title, text, image, answer, hint }) => {
-	const [isPuzzleVisible, setIsPuzzleVisible] = useState(true);
-	const [userAnswer, setUserAnswer] = useState('');
-	const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
-	const [showHint, setShowHint] = useState(false);
-	
-	const handleCancelClick = () => {
-		setIsPuzzleVisible(false);
-	};
-
-	const handleInputChange = (e) => {
-		setUserAnswer(e.target.value);
-	};
-
-	const handleCheckAnswer = () => {
-		const isNumeric = !isNaN(parseFloat(userAnswer)) && isFinite(userAnswer);
-		setIsAnswerCorrect(isNumeric);
-
-		socket.emit(ClientToServer.Submit, { userAnswer, isNumeric });
-	};
-
-	const handleShowHint = () => {
-		setShowHint(true);
-	};
-
-	useEffect(() => {
-		const handleServerSubmit = (data) => {
-			setIsAnswerCorrect(data.isAnswerCorrect);
-			setShowHint(data.showHint);
-		};
-
-		socket.on(ClientToServer.Submit, handleServerSubmit);
-
-		return () => {
-			socket.off(ClientToServer.Submit, handleServerSubmit);
-		};
-	}, []);
-
-	if (!isPuzzleVisible) {
-		return null;
-	}
-
-	return (
-		<Html>
-			<div
-				style={{
-					position: 'absolute',
-					transform: 'translate(-50%, 0%)',
-					top: '50%',
-					left: '50%',
-					padding: '15px',
-					background: 'white',
-					borderRadius: '5px',
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-				}}
-			>
-				<h1>{title}</h1>
-				<p>{text}</p>
-				<img src={`/models/models/${image}.png`} alt="Puzzle" />
-
-				<input
-					type="text"
-					value={userAnswer}
-					onChange={handleInputChange}
-					placeholder="Enter your answer"
-					style={{ margin: '10px 0', padding: '5px' }}
-				/>
-
-				<button onClick={handleCheckAnswer} style={{ margin: '5px 0' }}>
-					Check Answer
-				</button>
-				{isAnswerCorrect && <p style={{ color: 'green' }}>Correct</p>}
-				{!isAnswerCorrect && <p style={{ color: 'red' }}>----</p>}
-
-				{!showHint && (
-					<button onClick={handleShowHint} style={{ margin: '10px 0' }}>
-						Get Hint
-					</button>
-				)}
-				{showHint && <p>{hint}</p>}
-
-				<button onClick={handleCancelClick} style={{ marginTop: '10px' }}>
-					Go Back
-				</button>
-			</div>
-		</Html>
-	);
-};
-
-Enigme.propTypes = {
-	title: PropTypes.string.isRequired,
-	text: PropTypes.string.isRequired,
-	image: PropTypes.string,
-	answer: PropTypes.number,
-	hint: PropTypes.string,
-};
-
-export default Enigme;
-
-*/
