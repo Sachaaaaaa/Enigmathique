84ac1dedffc98fab878a651391601ea38c97bca2
Sacha Wos <sacha.wos@etu.univ-grenoble-alpes.fr>
2024-01-26 08:53:45 +0100

    fix(api): corrrectio de la vérification d'appartenance à une classe

--- enigmathique/backend/src/controllers/student.controller.js
+++ enigmathique/backend/src/controllers/student.controller.js
@@ -16,6 +16,16 @@ const { baseSchema } = require('./validationSchemas');
 // Fonction vérifiant si une classe, à partir de son id, appartiant au professeur
 async function isClassBelongsProfessor(idCourse, req) {
 
+	// Récupère la classe en question
+	const course = await Course.findOne({ where: { id: idCourse} });
+
+	// Vérifie que l'élève existe bien
+	if(!course){
+		const error = new Error("La classe n'existe pas.");
+		error.statusCode = 404;  
+		throw error;
+	}
+		
 	// Récupère toutes les classes du professeur courant
 	const courses = await Course.findAll({ where: { idProfessor: req.tokenId } });
 
