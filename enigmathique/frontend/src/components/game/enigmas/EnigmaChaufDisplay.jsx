6b9d0d3ab5e14b97a5f5b12c432a01799c430c0f
Raph255 <raphael.pivot2505@gmail.com>
2024-01-26 09:51:54 +0100

    style(web): boutons chauffe stylisés et fonctionnels

--- enigmathique/frontend/src/components/game/enigmas/EnigmaChaufDisplay.jsx
+++ enigmathique/frontend/src/components/game/enigmas/EnigmaChaufDisplay.jsx
@@ -8,6 +8,9 @@ import TemperatureButton from './TemperatureButton';
 const EnigmaChaufDisplay = ({ handleSubmitAnswer, handleAskHint, description, hint,isSolved, image, stepButton }) => {
 	const [userAnswer, setUserAnswer] = useState('');
 	const [temperature, setTemperature] = useState(0);
+	const [firstBtnAngle, setFirstBtnAngle] = useState(0);
+	const [secondBtnAngle, setSecondBtnAngle] = useState(0);
+	const [thirdBtnAngle, setThirdBtnAngle] = useState(0);
 
 	const handleInputChange = (event) => {
 		setUserAnswer(event.target.value);
@@ -17,8 +20,19 @@ const EnigmaChaufDisplay = ({ handleSubmitAnswer, handleAskHint, description, hi
 		setUserAnswer(temperature.toString());
 	}, [temperature]);
 
-	const handleRotation = (step) => {
+	const handleRotation = (step,idBtn) => {
 		setTemperature(temperature + step);
+		switch (idBtn) {
+			case 1:
+				setFirstBtnAngle(firstBtnAngle + 20);
+				break;
+			case 2:
+				setSecondBtnAngle(secondBtnAngle - 20);
+				break;
+			case 3:
+				setThirdBtnAngle(thirdBtnAngle + 20);
+				break;
+		}
 	}
 
 	return (
@@ -35,15 +49,31 @@ const EnigmaChaufDisplay = ({ handleSubmitAnswer, handleAskHint, description, hi
 			/>
 
 			{/* Bouton tournant */}
-			<div className='flex justify-around px-10 bg-black '>
-				<TemperatureButton step={stepButton} onClick={() =>handleRotation(stepButton)} isSolved={isSolved}/>
-				<div className='grow relative h-[100px]'>
-				<div className="absolute top-[50%] left-[50%] " style={{transform: `translate(-40%, -50%))` }}>
-					<button  disabled={isSolved} onClick={handleRotation} className='w-16 h-16 p-2 bg-black-color rounded-full cursor-pointer disabled:opacity-50'>
-						<img src={termos} alt="img Termos" />
-					</button>
+			<div className='flex justify-around px-5 bg-[#2B2B2B] '>
+				<div className='grow relative h-[100px] '>
+					<div className="absolute top-[50%] left-[50%] flex flex-col justify-center items-center " style={{transform: `translate(-40%, -50%)` }}>
+						<button disabled={isSolved} onClick={() =>handleRotation(stepButton,1)} className='w-16 h-16 p-1 bg-black-color rounded-full cursor-pointer disabled:opacity-50'>
+							<img src={termos} alt="img Termos" style={{transform : `rotate(${firstBtnAngle}deg)` }} />
+							</button>
+							<p className='text-white text-sm text-center'> {stepButton < 0 ? "- ": "+ " }{stepButton}</p>
+					</div>
 				</div>
-			</div>
+				<div className='grow relative h-[100px] '>
+					<div className="absolute top-[50%] left-[50%] flex flex-col justify-center items-center " style={{transform: `translate(-40%, -50%)` }}>
+						<button disabled={isSolved} onClick={() =>handleRotation(-1,2)} className='w-16 h-16 p-2 bg-black-color rounded-full cursor-pointer disabled:opacity-50'>
+							<img src={termos} alt="img Termos" style={{transform : `rotate(${secondBtnAngle}deg)` }} />
+						</button>
+						<p className='text-white text-sm text-center'> - 1</p>
+					</div>
+				</div>
+				<div className='grow relative h-[100px] '>
+					<div className="absolute top-[50%] left-[50%] flex flex-col justify-center items-center " style={{transform: `translate(-40%, -50%)` }}>
+						<button disabled={isSolved} onClick={() =>handleRotation(1,3)} className='w-16 h-16 p-2 bg-black-color rounded-full cursor-pointer disabled:opacity-50'>
+							<img src={termos} alt="img Termos" style={{transform : `rotate(${thirdBtnAngle}deg)` }} />
+						</button>
+						<p className='text-white text-sm text-center'> + 1</p>
+					</div>
+				</div>
 			</div>
 			{(!hint && !isSolved) && (
 				<HintButton onClick={() => handleAskHint()} />
