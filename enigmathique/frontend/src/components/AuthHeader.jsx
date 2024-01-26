642c142dd1d27b79b98065101f5c7387fcc76176
Yvain Dalban <yvain.dalban@etu.univ-grenoble-alpes.fr>
2024-01-26 17:00:53 +0100

    docs (web): commentaires AuthHeader

--- enigmathique/frontend/src/components/AuthHeader.jsx
+++ enigmathique/frontend/src/components/AuthHeader.jsx
@@ -3,6 +3,12 @@ import PropTypes from 'prop-types';
 import logo from '../assets/img/logo-name-enigmathique-black.png';
 import {Link} from 'react-router-dom';
 
+/**
+ * Entête des pages d'authentification + CGU
+ * @param title titre de la page
+ * @returns {Element}
+ * @constructor
+ */
 const AuthHeader = ({ title }) => {
     return (
         <header className='topbar-container flex justify-start h-[75px] bg-white'>
