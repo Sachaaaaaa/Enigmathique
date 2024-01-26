a6607b4f287cad959d805aee589b0efc858fc1a0
Yvain Dalban <yvain.dalban@etu.univ-grenoble-alpes.fr>
2024-01-26 17:06:12 +0100

    docs (web): correction import

--- enigmathique/frontend/src/components/AuthHeader.jsx
+++ enigmathique/frontend/src/components/AuthHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import logo from '../assets/img/logo-name-enigmathique-black.png';
+import logo from 'assets/img/logo-name-enigmathique-black.png';
 import {Link} from 'react-router-dom';
 
 /**
--- enigmathique/frontend/src/components/EmptyPage.jsx
+++ enigmathique/frontend/src/components/EmptyPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiInbox } from "react-icons/fi";
+import { FiInbox } from 'react-icons/fi';
 import PropTypes from 'prop-types';
 
 /**
